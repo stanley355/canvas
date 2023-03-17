88d717dbe79d93f761f60273a21547864005a895
Stanley Winata <winatastanley355@gmail.com>
2023-03-17 11:35:06 +0700

    The first file in the diff is the ResultCard component from the Scholar module. The diff shows that the component has been updated to use the new FaQuoteLeft icon from React-icons, and that the citation information has been moved into a new flex container. The second file is the ResultSkeleton component from the Scholar module. The diff shows that the component has been updated to use the new ResultCard component, and that the organicResults prop has been added.

--- src/modules/scholar/components/ResultCard/ResultCard.tsx
+++ src/modules/scholar/components/ResultCard/ResultCard.tsx
@@ -1,59 +1,20 @@
-import React from 'react';
-import Link from 'next/link';
-import {FaQuoteLeft} from 'react-icons/fa';
+import React from "react";
+import Link from "next/link";
+import { FaQuoteLeft } from "react-icons/fa";
 
-const ResultCard = ({result}:any) => (
-    <div className="p-4 border-b border-white">
-        <div>
-
-        <Link href={result.link} className="font-semibold text-xl underline">{result.title}</Link>
-        <div>
-            <FaQuoteLeft />
-            <span>{result.cited_by.total}</span>
-        </div>
-        </div>
-        {/* TODO: Create scholar author page */}
-        <div className='italic'>
-        {result.publication_info.summary}
-        </div>
-        <div>{result.snippet}</div>
+const ResultCard = ({ result }: any) => (
+  <div className="p-4 border-b border-white lg:w-2/3">
+    <Link href={result.link} className="font-semibold text-xl text-blue-200 underline">
+      {result.title}
+    </Link>
+    <div className="flex flex-row items-center">
+      <FaQuoteLeft className="mr-2"/>
+      <span>Cited: {result.inline_links.cited_by.total}</span>
     </div>
+    {/* TODO: Create scholar author page */}
+    <div className="italic">{result.publication_info.summary}</div>
+    <div>{result.snippet}</div>
+  </div>
 );
 
 export default ResultCard;
-
-// {
-//     "position": 0,
-//     "title": "The impact of coffee on health",
-//     "result_id": "sWzmct-yYzgJ",
-//     "link": "https://www.sciencedirect.com/science/article/pii/S0378512213000479",
-//     "snippet": "… , where pressed ground coffee beans are used directly, in either espresso or coffee pot so … espresso or coffee pots, where boiled water is directly passed through pressed coffee powder …",
-//     "publication_info": {
-//         "summary": "A Cano-Marquina, JJ Tarín, A Cano - Maturitas, 2013 - Elsevier",
-//         "authors": [
-//             {
-//                 "name": "A Cano-Marquina",
-//                 "link": "https://scholar.google.com/citations?user=nMS5yswAAAAJ&hl=en&oi=sra",
-//                 "serpapi_scholar_link": "https://serpapi.com/search.json?author_id=nMS5yswAAAAJ&engine=google_scholar_author&hl=en",
-//                 "author_id": "nMS5yswAAAAJ"
-//             }
-//         ]
-//     },
-//     "inline_links": {
-//         "serpapi_cite_link": "https://serpapi.com/search.json?engine=google_scholar_cite&q=sWzmct-yYzgJ",
-//         "cited_by": {
-//             "total": 439,
-//             "link": "https://scholar.google.com/scholar?cites=4063287961593474225&as_sdt=2005&sciodt=0,5&hl=en",
-//             "cites_id": "4063287961593474225",
-//             "serpapi_scholar_link": "https://serpapi.com/search.json?as_sdt=2005&cites=4063287961593474225&engine=google_scholar&hl=en"
-//         },
-//         "related_pages_link": "https://scholar.google.com/scholar?q=related:sWzmct-yYzgJ:scholar.google.com/&scioq=coffee&hl=en&as_sdt=0,5",
-//         "serpapi_related_pages_link": "https://serpapi.com/search.json?as_sdt=0%2C5&engine=google_scholar&hl=en&q=related%3AsWzmct-yYzgJ%3Ascholar.google.com%2F",
-//         "versions": {
-//             "total": 8,
-//             "link": "https://scholar.google.com/scholar?cluster=4063287961593474225&hl=en&as_sdt=0,5",
-//             "cluster_id": "4063287961593474225",
-//             "serpapi_scholar_link": "https://serpapi.com/search.json?as_sdt=0%2C5&cluster=4063287961593474225&engine=google_scholar&hl=en"
-//         }
-//     }
-// }
--- src/modules/scholar/components/ResultSkeleton/ResultSkeleton.tsx
+++ src/modules/scholar/components/ResultSkeleton/ResultSkeleton.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaBookOpen } from "react-icons/fa";
+import ResultCard from "../ResultCard";
 
 interface IScholarResultSkeleton {
   organicResults: any[];
@@ -8,14 +9,19 @@ interface IScholarResultSkeleton {
 const ScholarResultSkeleton = (props: IScholarResultSkeleton) => {
   const { organicResults } = props;
 
-//   console.log(organicResults);
   return (
     <div>
       <div className="p-4 border-b border-white flex flex-row items-center">
         <FaBookOpen className="text-lg mr-2" />
         <span>Scholar</span>
       </div>
-      
+      <div className="container mx-auto">
+        {organicResults.map((res) => (
+          <div key={res.position}>
+            <ResultCard result={res} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
