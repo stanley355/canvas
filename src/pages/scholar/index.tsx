13238b48176b13e473e7567300080bd664145e9e
Stanley Winata <winatastanley355@gmail.com>
2023-03-17 10:55:49 +0700

    The diff shows the changes made to the 'src/pages/scholar/index.tsx' file.
    The first change is on line 29, where the 'Router' import is replaced with an 'axios' import.
    
    The second change is on line 50, where a new prop is added to the 'ScholarPage' component. This prop is called 'searchResult', and it is used to store the results of a search made using the 'axios' module.
    
    The third and final change is on line 64, where the 'structuredClone' function is used to clone the 'serpRes.data.organic_results' object. This ensures that the original data is not modified when it is passed to the 'searchResult' prop.

--- src/pages/scholar/index.tsx
+++ src/pages/scholar/index.tsx
@@ -1,29 +1,50 @@
 import React from "react";
-import Router from "next/router";
+import axios from "axios";
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import ScholarPageLayout from "@/modules/scholar/components/layout";
 import ScholarSearchBox from "@/modules/scholar/components/SearchBox";
 
-const ScholarPage = () => {
+const ScholarPage = (props:any) => {
+  const {query, searchResult}  = props;
+
   return (
     <ScholarPageLayout>
-      <h1 className="font-bold text-3xl lg:text-4xl text-center">Scholar Data</h1>
+      <h1 className="font-bold text-3xl lg:text-4xl text-center">
+        Scholar Data
+      </h1>
       <ScholarSearchBox />
+      <h3 className="text-lg text-center mt-4">Knowledge is Power</h3>
     </ScholarPageLayout>
   );
 };
 
 export const getServerSideProps: GetServerSideProps = async (
-    context: GetServerSidePropsContext
-  ) => {
-    const { query } = context;
-  
-    return {
-      props: {
-        query,
-      },
-    };
+  context: GetServerSidePropsContext
+) => {
+  const { query } = context;
+  let searchResult = [];
+
+  if (query && query.q) {
+    const apiURL = new URL(
+      `${process.env.NEXT_PUBLIC_BASE_URL}api/serp/scholar`
+    );
+    Object.keys(query).forEach((key) => {
+      apiURL.searchParams.set(key, String(query[key]));
+    });
+
+    const serpRes: any = await axios.get(String(apiURL));
+
+    if (serpRes && serpRes.data) {
+      searchResult = structuredClone(serpRes.data.organic_results);
+    }
+  }
+
+  return {
+    props: {
+      query,
+      searchResult
+    },
   };
-  
+};
 
 export default ScholarPage;
