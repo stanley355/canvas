5bc9f22eec643c3bf515ca29cf83b924dda0484a
Stanley Winata <winatastanley355@gmail.com>
2023-03-17 10:09:51 +0700

    The diff shows the changes made to the file src/pages/api/serp/scholar.ts.
    In the first change, the import for axios is added. In the second change, the SERPAPI_URL and SERPAPI_KEY are used to construct a URL for the search. In the third change, the try-catch block is added to handle errors.

--- src/pages/api/serp/scholar.ts
+++ src/pages/api/serp/scholar.ts
@@ -1,24 +1,17 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import axios from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
 
-const serpScholarAPI= async (req: NextApiRequest, res: NextApiResponse) => {
-    const url = new URL(`${process.env.SERPAPI_URL}search`);
-    url.searchParams.set('api_key', String(process.env.SERPAPI_KEY));
-    url.searchParams.set('engine', 'google_scholar');
-//   const URL = `${process.env.SERPAPI_URL}search?api_key=4d1934e56d6edeb90c1096d0bdb1600cf7569fc76a4067270aea24511f11558c&engine=google_scholar`;
+const serpScholarAPI = async (req: NextApiRequest, res: NextApiResponse) => {
+  const url = new URL(`${process.env.SERPAPI_URL}search`);
+  url.searchParams.set("api_key", String(process.env.SERPAPI_KEY));
+  url.searchParams.set("engine", "google_scholar");
 
-//   let requestOptions: RequestInit = prepareHTTPConfig(req);
-//   requestOptions.body = JSON.stringify(req.body);
-
-//   let response;
-//   try {
-//     response = await HTTPClient(URL, requestOptions);
-//   } catch (err) {
-//     response = errorHandler(err);
-//   }
-
-//   res.statusCode = response && response.code ? response.code : 200;
-//   await res.setHeader('Content-Type', 'application/json');
-//   res.json(response);
+  try {
+    const response = await axios.get(String(url));
+    res.json(response);
+  } catch (error) {
+    res.json(error);
+  }
 };
 
 export default serpScholarAPI;
