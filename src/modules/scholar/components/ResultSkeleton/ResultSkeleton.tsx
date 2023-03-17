3dbbbf6636d3d18bacff67d2e2d00cd4c99ba37b
Stanley Winata <winatastanley355@gmail.com>
2023-03-17 11:11:56 +0700

    Styling

--- src/modules/scholar/components/ResultSkeleton/ResultSkeleton.tsx
+++ src/modules/scholar/components/ResultSkeleton/ResultSkeleton.tsx
@@ -4,8 +4,8 @@ import { FaBookOpen } from "react-icons/fa";
 const ScholarResultSkeleton = () => {
   return (
     <div>
-      <div className="p-4">
-        <FaBookOpen />
+      <div className="p-4 border-b border-white flex flex-row items-center">
+        <FaBookOpen className="text-lg mr-2" />
         <span>Scholar</span>
       </div>
     </div>
