b3153c13b6c75b5f8f09b8737620db4ca320481d
Stanley Winata <winatastanley355@gmail.com>
2023-03-18 14:44:03 +0700

    The first diff is for the Layout.tsx file. This file has been updated to include a new import, useMobileScreen, as well as new components, MobileView and DesktopView. MobileView is for mobile devices and DesktopView is for desktop devices.
    The second diff is for the index.tsx file. This file has been updated to include a new prop, getServerSideProps. This prop is used to get data from the server and then render it on the page.

--- src/modules/news/components/Layout.tsx
+++ src/modules/news/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import { FaBuffer, FaUserCircle } from "react-icons/fa";
+import useMobileScreen from "@/common/lib/isMobile";
 import NewsSearchBox from "./SearchBox";
 import Footer from "@/common/components/Footer";
 
@@ -12,21 +13,33 @@ interface INewsPageLayout {
 const NewsPageLayout = (props: INewsPageLayout) => {
   const { query, children } = props;
 
+  const isMobile = useMobileScreen();
+
+  const MobileView = () => (
+    <div className="flex flex-row items-center justify-between">
+      <button type="button" className="mr-2">
+        <FaBuffer className="text-3xl" />
+      </button>
+      <div className="text-2xl italic">News Data</div>
+      <Link href="/login" passHref>
+        <FaUserCircle className="text-3xl" />
+      </Link>
+    </div>
+  );
+
+  const DesktopView = () => (
+    <div className="flex flex-row items-center">
+      <div className="text-2xl italic mr-4">News Data</div>
+      <NewsSearchBox query={query} />
+    </div>
+  );
+
   return (
     <section className="relative">
       <header className="py-4 px-2 border-b border-white">
-        <div className="flex flex-row items-center justify-between">
-          <button type="button" className="mr-2">
-            <FaBuffer className="text-3xl" />
-          </button>
-          <div className="text-2xl italic">News Data</div>
-          <Link href="/login" passHref>
-            <FaUserCircle className="text-3xl" />
-          </Link>
-        </div>
+        {isMobile ? <MobileView /> : <DesktopView />}
       </header>
-      <NewsSearchBox query={query} />
-      <main className="min-h-screen">{children}</main>
+      <main className="min-h-screen container mx-auto">{children}</main>
       <Footer />
     </section>
   );
--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import NewsPageLayout from '@/modules/news/components/Layout';
+import React from "react";
+import NewsPageLayout from "@/modules/news/components/Layout";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
 
 const NewsPage = () => {
-    return (
-        <NewsPageLayout query={{}}>
-            hi
-        </NewsPageLayout>
-    )
+  return <NewsPageLayout query={{}}>hi</NewsPageLayout>;
+};
+
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  context.res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=10, stale-while-revalidate=59"
+  );
+
+  return {
+    props: {},
+  };
 };
 
 export default NewsPage;
