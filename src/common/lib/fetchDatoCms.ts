6156455c381f5c93d4a05d46c9f7e1962df5feae
Stanley Winata <winatastanley355@gmail.com>
2023-03-18 09:52:44 +0700

    Add fetchDatoCms

---
+++ src/common/lib/fetchDatoCms.ts
@@ -0,0 +1,20 @@
+import { GraphQLClient } from "graphql-request";
+
+const fetchDatoCms = ({
+  query,
+  variables,
+}: {
+  query: string;
+  variables: any;
+}) => {
+  const headers = {
+    authorization: `Bearer ${process.env.DATOCMS_TOKEN}`,
+    "X-Exclude-Invalid": "true",
+  };
+
+  const url = String(process.env.DATOCMS_GRAPHQL_URL);
+  const client = new GraphQLClient(url, {
+    headers,
+  });
+  return client.request(query, variables);
+};
