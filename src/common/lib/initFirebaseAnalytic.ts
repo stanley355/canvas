05d7b0c63feca5702e9d733c4c584ab4781a18e2
Stanley Winata <winatastanley355@gmail.com>
2023-03-20 15:47:31 +0700

    Add firebase

--- src/common/lib/initFirebaseAnalytic.ts
+++ src/common/lib/initFirebaseAnalytic.ts
@@ -1,16 +1,25 @@
 import getConfig from "next/config";
-import axios from "axios";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
-const { BASE_URL, APP_ENV } = getConfig().publicRuntimeConfig;
+const { APP_ENV, FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER, FIREBASE_APP_ID, FIREBASE_MEASUREMENT_ID } = getConfig().publicRuntimeConfig;
 
-const initFirebaseAnalytic = async () => {
-  const configURL = `${BASE_URL}api/firebase-config/`;
-  const firebaseConfig: any = await axios.get(configURL, {});
+const initFirebaseAnalytic = () => {
+  const firebaseConfig = {
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESSAGING_SENDER,
+    appId: FIREBASE_APP_ID,
+    measurementId: FIREBASE_MEASUREMENT_ID,
+  };
+
+  console.log(firebaseConfig);
 
   if (APP_ENV !== "develop") {
     const app = initializeApp(firebaseConfig);
+    console.log(app);
     getAnalytics(app);
   }
 };
