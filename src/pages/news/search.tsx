7209eb01b1d12e15ada4f15b6eb85e2000fefac6
Stanley Winata <winatastanley355@gmail.com>
2023-03-20 12:02:36 +0700

    The diff shows the addition of two new functions,  and , as well as the removal of the  function.
    These changes are all related to caching data in Redis, a key-value store. The  function fetches data from Redis, and the  function stores data in Redis. The  function is no longer needed because the data is being cached and does not need to be recalculated every time the page is loaded.

--- src/pages/news/search.tsx
+++ src/pages/news/search.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import NewsPageLayout from "@/modules/news/components/Layout";
+import { getFromRedis } from "@/common/lib/getFromRedis";
+import { storeToRedis } from "@/common/lib/storeToRedis";
 import { calculatePageLoadTime } from "@/common/components/calculatePageLoadTime";
 import { getSearchNewsData } from "@/modules/news/lib/getSearchNewsData";
 
@@ -10,8 +12,7 @@ const NewsSearchPage = (props: any) => {
   return (
     <NewsPageLayout query={query}>
       <div className="border-b text-center py-2">
-        Showing {nyt.length + theGuardian.length + newsAPI.length} News (
-        {calculatePageLoadTime()} s)
+        Showing {nyt.length + theGuardian.length + newsAPI.length} News
       </div>
     </NewsPageLayout>
   );
@@ -21,11 +22,35 @@ export const getServerSideProps: GetServerSideProps = async (
   context: GetServerSidePropsContext
 ) => {
   const { query } = context;
-  const newsData: any = await getSearchNewsData(String(query.q));
 
-  let nyt = newsData[0].value;
-  let theGuardian = newsData[1].value;
-  let newsAPI = newsData[2].value;
+  //   TODO: Remove static redis cache
+  let nyt = [];
+  let theGuardian = [];
+  let newsAPI = [];
+
+  const REDIS_KEY = "news:search";
+
+  const prevData = await getFromRedis(REDIS_KEY);
+
+  // TODO: Reduce data fetching
+  if (prevData && prevData.length > 0) {
+    nyt = prevData[0].value;
+    theGuardian = prevData[1].value;
+    newsAPI = prevData[2].value;
+  } else {
+    const newsData = await getSearchNewsData(String(query.q));
+    const storedData = await storeToRedis(REDIS_KEY, newsData);
+
+    nyt = storedData[0].value;
+    theGuardian = storedData[1].value;
+    newsAPI = storedData[2].value;
+  }
+
+  //   const newsData: any = await getSearchNewsData(String(query.q));
+
+  //   let nyt = newsData[0].value;
+  //   let theGuardian = newsData[1].value;
+  //   let newsAPI = newsData[2].value;
 
   context.res.setHeader(
     "Cache-Control",
