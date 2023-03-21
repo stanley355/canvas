d247e55ddbdc374a96c95bce06cbb37180d53361
Stanley Winata <winatastanley355@gmail.com>
2023-03-21 13:31:04 +0700

    1. The diff adds a new import for the useDesktopScreen hook, and adds a new constant for NEWS_SECTIONS.
    2. The diff also adds a new Button component to the layout.
    
    3. Finally, the diff adds a new div to the layout which contains a list of news sections.

--- src/modules/news/components/Layout.tsx
+++ src/modules/news/components/Layout.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Router from "next/router";
 import { FaNewspaper } from "react-icons/fa";
+import { useDesktopScreen } from "@/common/hooks/useDesktopScreen";
+import { NEWS_SECTIONS } from "../lib/constant";
 import SearchNavbar from "@/common/components/SearchNavbar";
 import Footer from "@/common/components/Footer";
+import Button from "@/common/components/Button";
 
 interface INewsPageLayout {
   query: any;
@@ -11,6 +14,8 @@ interface INewsPageLayout {
 
 const NewsPageLayout = (props: INewsPageLayout) => {
   const { query, children } = props;
+
+  const isDesktop = useDesktopScreen();
 
   return (
     <section className="relative">
@@ -21,6 +26,21 @@ const NewsPageLayout = (props: INewsPageLayout) => {
         searchPlaceHolder={query && query.q ? query.q : "What's my news today?"}
         onSearchSubmit={(val) => Router.push(`/news/search?q=${val}`)}
       />
+      {isDesktop && (
+        <div className="w-full border-b py-2">
+          <div className="container mx-auto flex flex-row justify-evenly text-lg">
+            {NEWS_SECTIONS.map((section) => (
+              <Button
+                type="link"
+                key={section.title}
+                href={section.href}
+                buttonClassName="p-2 hover:text-black hover:bg-white"
+                title={section.title}
+              />
+            ))}
+          </div>
+        </div>
+      )}
       <main className="min-h-screen container mx-auto">{children}</main>
       <Footer />
     </section>
---
+++ src/modules/news/lib/constant.ts
@@ -0,0 +1,47 @@
+export const NEWS_SECTIONS = [
+    {
+        title: 'World',
+        href: '/news',
+    },
+    {
+        title: 'Business',
+        href: '/news/business/'
+    },
+    {
+        title: 'Sports',
+        href: '/news/sports/'
+    },
+    {
+        title: 'Politics',
+        href: '/news/politics/'
+    },
+    {
+        title: 'Games',
+        href: '/news/games/'
+    },
+    {
+        title: 'Opinion',
+        href: '/news/opinion/'
+    },
+    {
+        title: 'Science',
+        href: '/news/science/'
+    },
+    {
+        title: 'Health',
+        href: '/news/health/'
+    },
+    {
+        title: 'Books',
+        href: '/news/books/'
+    },
+    {
+        title: 'Food',
+        href: '/news/food/'
+    },
+    {
+        title: 'Travel',
+        href: '/news/travel/'
+    }
+
+]
