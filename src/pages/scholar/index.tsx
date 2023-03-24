722f17d122188f08cf29092a992fb4323c263f34
Stanley Winata <winatastanley355@gmail.com>
2023-03-24 14:36:23 +0700

    The diff shows the changes made to the src/pages/scholar/index.tsx file.
    The changes include removing the fetchDatoCms function and the SCHOLAR_DATO_SEO_QUERY constant, and adding the useRouter hook.
    
    The diff also includes changes to the ScholarPage component, including removing the query and seo props and adding the router prop.

--- src/pages/scholar/index.tsx
+++ src/pages/scholar/index.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import { GetServerSideProps, GetServerSidePropsContext } from "next";
-import axios from "axios";
-
-import { fetchDatoCms } from "@/common/lib/fetchDatoCms";
-import { SCHOLAR_DATO_SEO_QUERY } from "@/modules/scholar/lib/query";
-
+import { useRouter } from "next/router";
 import MetaSEO from "@/common/components/MetaSEO";
 import ScholarPageLayout from "@/modules/scholar/components/Layout";
 import ScholarHome from "@/modules/scholar/components/ScholarHome";
@@ -12,12 +7,13 @@ import ScholarSearchSkeleton from "@/modules/scholar/components/ScholarSearchSke
 
 const ScholarPage = (props: any) => {
   const { query, seo, serpScholar } = props;
+  const router = useRouter();
 
   return (
     <ScholarPageLayout query={query}>
       <MetaSEO seo={seo} />
 
-      {query && query.q ? (
+      {router.query && router.query.q ? (
         <ScholarSearchSkeleton
           serpPaperList={serpScholar?.organic_results ?? []}
         />
@@ -28,39 +24,5 @@ const ScholarPage = (props: any) => {
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  const { query } = context;
-  let serpScholar = null;
-
-  const datoSEO: any = await fetchDatoCms({
-    query: SCHOLAR_DATO_SEO_QUERY,
-    variables: "",
-  });
-
-  if (query && query.q) {
-    const url = new URL(`${process.env.NEXT_PUBLIC_BASE_URL}api/serp/scholar`);
-    url.searchParams.set("q", String(query.q));
-    url.searchParams.set("num", "20");
-
-    const { data } = await axios.get(String(url));
-
-    serpScholar = data;
-  }
-
-  context.res.setHeader(
-    "Cache-Control",
-    "public, s-maxage=10, stale-while-revalidate=59"
-  );
-
-  return {
-    props: {
-      query,
-      seo: datoSEO?.scholar?.seo ?? null,
-      serpScholar,
-    },
-  };
-};
 
 export default ScholarPage;
