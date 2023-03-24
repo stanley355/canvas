7a2e6e1e60d5eb8323bcc0d63ca060aeeec4ab4d
Stanley Winata <winatastanley355@gmail.com>
2023-03-24 18:30:29 +0700

    The first thing to note is that this diff is for multiple files, not just one. The main file being changed here is the index.tsx file, but there are also changes to the MobileBar and DesktopBar files.
    In the index.tsx file, the biggest change is the removal of the SerpScholarMobileBar component and the addition of the ScholarMobileBar component. This change is also reflected in the DesktopBar file.
    
    The SerpScholarMobileBar and DesktopBar components are used to display the search results in a mobile or desktop format, respectively. The ScholarMobileBar component is a new component that combines the functionality of both the SerpScholarMobileBar and DesktopBar components.
    
    The other changes in the index.tsx file are mostly small changes to the code that don't really change the functionality of the file.
    
    In the MobileBar and DesktopBar files, the only change is the removal of the SerpScholarMobileBar and DesktopBar components, respectively. These components have been replaced by the ScholarMobileBar component in the index.tsx file.

--- src/modules/scholar/components/ScholarMobileBar.tsx
+++ src/modules/scholar/components/ScholarMobileBar.tsx
@@ -1,15 +1,17 @@
+
 import React from "react";
 import { FaGraduationCap, FaChevronUp, FaChevronDown } from "react-icons/fa";
 import Button from "@/common/components/Button";
 
-interface ISerpScholarMobileBar {
+interface IScholarMobileBar {
+    title: string;
   showTable: boolean;
   resultLength: number;
   onClick: () => void;
 }
 
-const SerpScholarMobileBar = (props: ISerpScholarMobileBar) => {
-  const { onClick, showTable, resultLength } = props;
+const ScholarMobileBar = (props: IScholarMobileBar) => {
+  const { onClick, title, showTable, resultLength } = props;
 
   return (
     <Button
@@ -19,7 +21,7 @@ const SerpScholarMobileBar = (props: ISerpScholarMobileBar) => {
     >
       <span className="flex flex-row items-center text-xl">
         <FaGraduationCap className="text-2xl" />
-        <span className="mx-2">Google Scholar</span>
+        <span className="mx-2">{title}</span>
         <span>({resultLength} Results)</span>
       </span>
       {showTable ? <FaChevronUp /> : <FaChevronDown />}
@@ -27,4 +29,4 @@ const SerpScholarMobileBar = (props: ISerpScholarMobileBar) => {
   );
 };
 
-export default SerpScholarMobileBar;
+export default ScholarMobileBar;
--- src/modules/scholar/components/SemanticScholar/index.tsx
+++ src/modules/scholar/components/SemanticScholar/index.tsx
@@ -4,10 +4,7 @@ import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { FaSpinner } from "react-icons/fa";
 
 import ScholarMobileBar from "../ScholarMobileBar";
-import SerpScholarDesktopBar from "./DesktopBar";
-import SerpScholarTable from "./SerpScholarTable";
 
-import { fetchSerpScholar } from "../../lib/fetchSerpScholar";
 import { useDesktopScreen } from "@/common/hooks/useDesktopScreen";
 
 const SerpScholar = () => {
@@ -15,37 +12,37 @@ const SerpScholar = () => {
   const router = useRouter();
   const isDesktop = useDesktopScreen();
 
-  const queryClient = useQueryClient();
+  //   const queryClient = useQueryClient();
 
-  const { isLoading, data } = useQuery({
-    queryKey: ["fetchSerpScholar"],
-    queryFn: () => fetchSerpScholar(String(router.query.q)),
-  });
+  //   const { isLoading, data } = useQuery({
+  //     queryKey: ["fetchSerpScholar"],
+  //     queryFn: () => fetchSerpScholar(String(router.query.q)),
+  //   });
 
-  const mutation = useMutation({
-    mutationFn: (filter: any) =>
-      fetchSerpScholar(String(router.query.q), {
-        key: filter.value[0],
-        value: filter.value[1],
-      }),
-    onSuccess: (newData) => {
-      queryClient.setQueryData(["fetchSerpScholar"], newData);
-    },
-  });
+  //   const mutation = useMutation({
+  //     mutationFn: (filter: any) =>
+  //       fetchSerpScholar(String(router.query.q), {
+  //         key: filter.value[0],
+  //         value: filter.value[1],
+  //       }),
+  //     onSuccess: (newData) => {
+  //       queryClient.setQueryData(["fetchSerpScholar"], newData);
+  //     },
+  //   });
 
   const SerpScholarResult = () => {
-    if (isLoading)
-      return (
-        <div className="py-4 flex items-center justify-center">
-          <FaSpinner className="animate-spin text-3xl" />
-        </div>
-      );
+    // if (isLoading)
+    //   return (
+    //     <div className="py-4 flex items-center justify-center">
+    //       <FaSpinner className="animate-spin text-3xl" />
+    //     </div>
+    //   );
 
-    if (data && data.organic_results) {
-      if (data.organic_results.length > 0) {
-        return <SerpScholarTable paperList={data.organic_results} />;
-      }
-    }
+    // if (data && data.organic_results) {
+    //   if (data.organic_results.length > 0) {
+    //     return <SerpScholarTable paperList={data.organic_results} />;
+    //   }
+    // }
     return (
       <div className="p-4 text-center text-xl border rounded-sm">
         No Results
@@ -56,6 +53,16 @@ const SerpScholar = () => {
   return (
     <div>
       {isDesktop ? (
+        <></>
+      ) : (
+        <ScholarMobileBar
+          title="Semantic Scholar"
+          showTable={showTable}
+          resultLength={0}
+          onClick={() => setShowTable(!showTable)}
+        />
+      )}
+      {/* {isDesktop ? (
         <SerpScholarDesktopBar
           showTable={showTable}
           onFilterChange={mutation.mutate}
@@ -65,17 +72,16 @@ const SerpScholar = () => {
           }
         />
       ) : (
-        <ScholarMobileBar
-          title="Google Scholar"
+        <SerpScholarMobileBar
           showTable={showTable}
           onClick={() => setShowTable(!showTable)}
           resultLength={
             data && data.organic_results ? data.organic_results.length : 0
           }
         />
-      )}
+      )} */}
       <div className="overflow-y-scroll lg:overflow-hidden">
-        {showTable && <SerpScholarResult />}
+        {/* {showTable && <SerpScholarResult />} */}
       </div>
     </div>
   );
