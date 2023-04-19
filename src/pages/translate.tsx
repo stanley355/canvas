4bc83fe2671ebaec1ada0187ccee535cf08cdabe
Stanley Winata <winatastanley355@gmail.com>
2023-04-19 15:32:12 +0700

    Translate

--- src/pages/translate.tsx
+++ src/pages/translate.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Select from "react-select";
 import { FaAngleDoubleRight } from "react-icons/fa";
+import Button from "@/common/components/Button";
 import Layout from "@/common/components/Layout";
 
 const LangTranslate = () => {
@@ -12,6 +13,32 @@ const LangTranslate = () => {
           <FaAngleDoubleRight className="w-2/12" />
           <Select className="w-5/12" placeholder="Select Lang" />
         </div>
+        <form onSubmit={() => {}}>
+            <div className="lg:flex lg:flex-row lg:gap-2 lg:mb-2">
+
+          <textarea
+            name="ori_lang_text"
+            id="ori_lang_textarea"
+            cols={30}
+            rows={10}
+            className="w-full border rounded-md bg-transparent p-2"
+            placeholder="..."
+          />
+          <textarea
+            name="target_lang_text"
+            id="target_lang_textarea"
+            cols={30}
+            rows={10}
+            className="w-full border rounded-md bg-transparent p-2"
+            placeholder="..."
+          />
+            </div>
+          <Button
+            type="submit"
+            title="Submit"
+            buttonClassName="w-full bg-white text-black py-2 text-md rounded-md font-semibold text-center hover:border hover:border-white hover:bg-black hover:text-white"
+          />
+        </form>
       </div>
     </Layout>
   );
