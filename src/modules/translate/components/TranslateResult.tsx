f185c80fd4e705af2b7eac1f23cd13875485db81
Stanley Winata <winatastanley355@gmail.com>
2023-04-20 09:26:55 +0700

    Trasnlate result skeleton

--- src/modules/translate/components/TranslateResult.tsx
+++ src/modules/translate/components/TranslateResult.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import Button from "@/common/components/Button";
 
-const TranslateComparison = () => {
-    return (
-        <div>
-           <textarea name="translate_result" id="translate_result_textarea" cols={30} rows={10} />
-        </div>
-    )
+const TranslateResult = () => {
+  return (
+    <div className="py-2">
+      <textarea
+        name="translate_result"
+        id="translate_result_textarea"
+        className="w-full rounded-md text-black"
+        cols={30}
+        rows={10}
+      />
+      <div className="grid grid-cols-2 gap-2">
+        <Button type="button" title="Copy" buttonClassName="w-full text-center p-2 bg-white text-black rounded-md" />
+        <Button type="link" title="Go to Top" wrapperClassName="flex items-center justify-center w-full" buttonClassName="w-full text-center p-2 bg-white text-black rounded-md" />
+      </div>
+    </div>
+  );
 };
 
-export default TranslateComparison;
+export default TranslateResult;
--- src/pages/translate.tsx
+++ src/pages/translate.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-import TranslateForm from "@/modules/translate/components/TranslateForm";
 import Layout from "@/common/components/Layout";
+import TranslateForm from "@/modules/translate/components/TranslateForm";
+import TranslateResult from "@/modules/translate/components/TranslateResult";
 
 const LangTranslate = () => {
   return (
     <Layout>
       <div className="container mx-auto px-2">
-      <h1 className="py-2 text-3xl">AI Translate (+Context) </h1>
+        <h1 className="py-2 text-3xl">AI Translate (+Context) </h1>
         <TranslateForm />
+        <TranslateResult />
       </div>
     </Layout>
   );
