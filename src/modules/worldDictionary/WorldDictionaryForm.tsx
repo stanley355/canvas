52f1e06b3fbac75447fc7df561aa03938d54e360
Stanley Winata <winatastanley355@gmail.com>
2023-04-22 10:10:38 +0700

    World dictionary form

--- src/modules/worldDictionary/WorldDictionaryForm.tsx
+++ src/modules/worldDictionary/WorldDictionaryForm.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
+import Select from "react-select";
 import Button from "@/common/components/Button";
+import { WORLD_DICTIONARY_ADDITION } from "./constant";
+import { LANGUAGE_LIST } from "../translate/constant";
 
 const WorldDictionaryForm = () => {
   return (
     <form onSubmit={() => {}} className="my-8">
-      <div className="flex flex-row items-center border border-white rounded-md">
+      <div className="flex flex-row items-center border border-white rounded-md mb-4">
         <FaSearch className="text-2xl m-2" />
         <input
           type="text"
@@ -15,6 +18,18 @@ const WorldDictionaryForm = () => {
           className="w-full bg-transparent border-l border-white p-2"
         />
       </div>
+      <Select
+        placeholder="Any additional topping? (You can choose more than one)"
+        options={WORLD_DICTIONARY_ADDITION}
+        className="text-black mb-4 bg-black"
+        isMulti
+        onChange={(option) => console.log(option)}
+      />
+      <Select
+        placeholder="Optional: Output Language"
+        options={LANGUAGE_LIST}
+        className="text-black"
+      />
       <Button
         type="submit"
         title="Submit"
---
+++ src/modules/worldDictionary/constant.ts
@@ -0,0 +1,14 @@
+export const WORLD_DICTIONARY_ADDITION = [
+    {
+        label: "Synonym",
+        value: "Synonym",
+    },
+    {
+        label: "Antonym",
+        value: "Antonym"
+    },
+    {
+        label: "Example Sentences",
+        value: "Example Sentences"
+    }
+]
