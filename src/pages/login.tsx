7f84a731d7fc6b5a5b9bc3d567b9ecd86e7ab4ce
Stanley Winata <winatastanley355@gmail.com>
2023-05-09 16:37:25 +0700

    Forgot password form

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import Layout from "@/common/components/Layout";
 import LoginForm from "@/modules/login/components/LoginForm";
 import RegisForm from "@/modules/login/components/RegisForm";
+import ForgotPasswordForm from "@/modules/login/components/ForgotPasswordForm";
+
 
 const LoginPage = () => {
   return (
     <Layout>
       <div className="container mx-auto flex items-center justify-center my-20 px-4">
         {/* <LoginForm /> */}
-        <RegisForm />
+        {/* <RegisForm /> */}
+        <ForgotPasswordForm />
       </div>
     </Layout>
   );
