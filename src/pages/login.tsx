89c86027b69f60094fad60fb3e04d550a8614858
Stanley Winata <winatastanley355@gmail.com>
2023-05-11 09:22:47 +0700

    login page seo

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import Layout from "@/common/components/Layout";
 import LoginForm from "@/modules/login/components/LoginForm";
+import MetaSEO from "@/common/components/MetaSEO";
 
 const LoginPage = () => {
+  const seo = {
+    title:
+      "LanguageAI - 10x Better Writing Check and Translation for All Languages",
+    description:
+      "Discover LanguageAI's Checkbot and Translation products that provide 10x better grammar and translation accuracy than Grammarly and Google Translate. Our AI-powered technology can check your writing and correct grammar and spelling errors in any language. Translate any language in the world with LanguageAI, and provide context to get accurate and nuanced translations. Experience LanguageAI, the best language tool for writers and communicators worldwide.",
+    url: process.env.NEXT_PUBLIC_BASE_URL,
+  };
 
   return (
     <Layout>
+      <MetaSEO seo={seo} />
       <div className="container mx-auto flex items-center justify-center my-20 px-4">
         <LoginForm />
       </div>
