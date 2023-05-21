b15130c194325b6f08f3a937101636b0326ee0c2
stan <winatastanley355@gmail.com>
2023-05-21 10:30:40 +0700

    login form skeleton

--- src/modules/login/components/LoginForm.tsx
+++ src/modules/login/components/LoginForm.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaSpinner } from "react-icons/fa";
 import { SiTaichilang } from "react-icons/si";
 import GoogleLoginBtn from "./GoogleLoginBtn";
+import Button from "@/common/components/Button";
 
 const LoginForm = () => {
+  const [hasSubmit, setHasSubmit] = useState(false);
+
   return (
-    <div className="rounded-md py-8 lg:w-1/3 mt-24 mx-auto flex flex-col items-center justify-center border border-white">
+    <div className="flex flex-col items-center justify-center p-4 w-full lg:w-1/3 lg:mx-auto">
       <h1 className="flex flex-row items-center justify-center text-2xl mb-2">
         <SiTaichilang />
         <span className="ml-2">LanguageAI Login</span>
@@ -12,10 +16,24 @@ const LoginForm = () => {
       <h2 className="text-2xl mb-4 font-semibold">
         Don&apos;t worry, it&apos;s always free!
       </h2>
-      <h3 className="text-lg font-semibold mb-2">
-        Sign Up / Login in One Click
+      <h3 className="mb-2">
+        Login with One Click
       </h3>
       <GoogleLoginBtn />
+      <div className="my-4">or</div>
+      <form action="" className="w-full">
+        <div className="flex flex-col mb-4">
+          <label htmlFor="email">Email*</label>
+          <input type="email" id='email_input' name='email' className='p-2 text-black rounded-sm' placeholder='myemail@email.com' />
+        </div>
+        <div className="flex flex-col mb-8">
+          <label htmlFor="password">Password*</label>
+          <input type="password" id='password_input' name='password' className='p-2 text-black rounded-sm' placeholder='******' />
+        </div>
+        <Button disabled={hasSubmit} type='submit' wrapperClassName='border border-white p-2 rounded-sm flex items-center justify-center bg-white text-black hover:bg-black hover:text-white' buttonClassName='w-full'>
+          {hasSubmit ? <FaSpinner className='animate-spin mx-auto' /> : "Login"}
+        </Button>
+      </form>
     </div>
   );
 };
