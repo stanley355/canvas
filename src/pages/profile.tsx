65e3527de315fd28e092d490784dcf79fcf40d5f
stan <winatastanley355@gmail.com>
2023-05-23 16:10:30 +0700

    Move profile balance

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 import Cookies from 'js-cookie';
-import Layout from '@/common/components/Layout';
 import jwtDecode from 'jwt-decode';
+import Layout from '@/common/components/Layout';
 import Button from '@/common/components/Button';
+import ProfileBalance from '@/modules/profile/components/ProfileBalance';
 
 interface IProfile {
   user: {
@@ -20,25 +21,13 @@ const Profile = (props: IProfile) => {
     window.location.href = "/";
   };
 
-
   return (
     <Layout>
       <div className='container mx-auto p-4 h-screen'>
         <div className='text-2xl'>{user.fullname}</div>
         <div>{user.email}</div>
 
-      <div className='mt-8 lg:w-1/3'>
-        <div className='font-semibold text-lg'>Oops you are running out of balance!</div>
-        <div className='border p-2 my-2'>
-          Rp 0
-        </div>
-        <div className='my-2'>*Add more balance you can access our Premium Translation and Checkbot (Better Result & Correction)</div>
-        <div>**You can even start Premium with Rp1000, we charge you by per word/token basis (Rp 1 per word)</div>
-        <div className='grid grid-cols-2 gap-4 my-4'>
-          <Button type='link' href='/premium' title='Premium' wrapperClassName='border border-white p-2 bg-white text-black text-center rounded font-semibold hover:bg-transparent hover:text-white' buttonClassName='w-full' />
-          <Button type='link' href='/balance' title='Balance' wrapperClassName='border border-white p-2 bg-white text-black text-center rounded font-semibold hover:bg-transparent hover:text-white' buttonClassName='w-full' />
-        </div>
-      </div>
+<ProfileBalance />
       <Button type='button' onClick={onLogoutClick} title='logout' wrapperClassName='mt-8 border border-white w-16 py-1 rounded text-center ' buttonClassName='w-full hover:underline'/>
     </div>
     </Layout >
