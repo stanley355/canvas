eadd2da1978ff59f80dd6b6895607a6914b27571
stan <winatastanley355@gmail.com>
2023-05-29 09:24:18 +0700

    va info

--- src/modules/profile/components/VAinfo.tsx
+++ src/modules/profile/components/VAinfo.tsx
@@ -6,14 +6,20 @@ interface IVAInfo {
 }
 
 const VAinfo = ({ info }: IVAInfo) => (
-  <div>
-    <div>Payment via {info.bank_name}</div>
-    <div>Amount: {info.amount}</div>
+  <div className='mt-4 text-lg'>
+    <div>Payment via: <i>{info.bank_name}</i> </div>
+    <div className='mt-2'>Amount:  <i>Rp{info.amount}</i> </div>
+    <div className='text-center lg:flex lg:flex-row lg:items-center lg:justify-center my-2'>
+
     <div>Virtual Account Number: </div>
-    <div>{info.virtual_account_number}</div>
+    <div className='lg:ml-2 italic underline text-blue-300'>{info.virtual_account_number}</div>
+    </div>
+    <div className='border border-white p-2 rounded flex flex-row mb-4 justify-center'>
+<div className='mr-2'>Pay Before:</div>
     <Countdown date={new Date(info.expired_date_utc)} />
+    </div>
     <div>Notes:</div>
-    <div>* Before payment: please don't refresh or quit this page</div>
+    <div>* Before payment: please don't refresh or close this page</div>
     <div>** After payment: please refresh the page to see your updated balance</div>
   </div>
 );
--- src/pages/topup.tsx
+++ src/pages/topup.tsx
@@ -3,6 +3,7 @@ import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import jwtDecode from "jwt-decode";
 import Layout from "@/common/components/Layout";
 import TopupForm from "@/modules/profile/components/TopupForm";
+import VAinfo from "@/modules/profile/components/VAinfo";
 import { fetchUserData } from "@/modules/profile/lib/fetchUserData";
 
 interface ITopup {
@@ -11,20 +12,20 @@ interface ITopup {
 
 const Topup = (props: ITopup) => {
   const { user } = props;
-  const [vaInfo, setVaInfo] = useState({});
+  const [vaInfo, setVaInfo] = useState<any>({});
 
   console.log(vaInfo);
 
-//   {
-//     "virtual_account_number": "8000000000010446",
-//     "created_date": "20230529090510",
-//     "expired_date": "20230529100510",
-//     "created_date_utc": "2023-05-29T02:05:10Z",
-//     "expired_date_utc": "2023-05-29T03:05:10Z",
-//     "how_to_pay_page": "https://sandbox.doku.com/how-to-pay/v2/doku-virtual-account/8000000000010446/LHidqYTdpkkclYaQckAl0MMh7l0c_Uwf9vZa5Fj07L8",
-//     "how_to_pay_api": "https://api-sandbox.doku.com/doku-virtual-account/v2/how-to-pay-api/8000000000010446/LHidqYTdpkkclYaQckAl0MMh7l0c_Uwf9vZa5Fj07L8",
-//     "bank_name": "BCA via DOKU"
-// }
+  //   {
+  //     "virtual_account_number": "8000000000010446",
+  //     "created_date": "20230529090510",
+  //     "expired_date": "20230529100510",
+  //     "created_date_utc": "2023-05-29T02:05:10Z",
+  //     "expired_date_utc": "2023-05-29T03:05:10Z",
+  //     "how_to_pay_page": "https://sandbox.doku.com/how-to-pay/v2/doku-virtual-account/8000000000010446/LHidqYTdpkkclYaQckAl0MMh7l0c_Uwf9vZa5Fj07L8",
+  //     "how_to_pay_api": "https://api-sandbox.doku.com/doku-virtual-account/v2/how-to-pay-api/8000000000010446/LHidqYTdpkkclYaQckAl0MMh7l0c_Uwf9vZa5Fj07L8",
+  //     "bank_name": "BCA via DOKU"
+  // }
 
   return (
     <Layout>
@@ -36,7 +37,7 @@ const Topup = (props: ITopup) => {
             Oops you are running out of balance!
           </div>
           <div className="border p-2 my-2">Current Balance: Rp {user.balance}</div>
-          <TopupForm user={user} dispatchVAinfo={setVaInfo} />
+          {vaInfo?.bank_name ? <VAinfo info={vaInfo} /> : <TopupForm user={user} dispatchVAinfo={setVaInfo} />}
         </div>
       </div>
     </Layout>
