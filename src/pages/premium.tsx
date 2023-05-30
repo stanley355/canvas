3dfc3720515a295639d21a4d6bc3c9299b4442e4
stan <winatastanley355@gmail.com>
2023-05-30 09:19:12 +0700

    Premium layout

---
+++ src/pages/premium.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '@/common/components/Layout';
+
+const Premium = () => {
+  return (
+    <Layout>
+      hi
+    </Layout>
+  )
+};
+
+export default Premium;
