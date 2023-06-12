d408f0c53aee910fa58d57c7dfaa138d371291f0
stan <winatastanley355@gmail.com>
2023-06-12 20:06:59 +0700

    Checkbot form skeleton

--- src/modules/premium/lib/constant.ts
+++ src/modules/premium/lib/constant.ts
@@ -1,10 +1,3 @@
-export const PREMIUM_OPTIONS = [
-  {
-    label: "Translate",
-    value: "translate"
-  },
-];
-
 export const PREMIUM_LANGUAGE_LIST = [
   {
     label: "None",
@@ -85,5 +78,64 @@ export const PREMIUM_LANGUAGE_LIST = [
   {
     label: "Vietnamese",
     value: "vi",
+  },
+];
+
+export const CHECKBOT_OPTIONS = [
+  {
+    label: "I'll give you my instruction",
+    value: "personal_instruction",
+  },
+  {
+    label: "Analyse Strength and Weakness",
+    value: "Analyze the strength and weakness from",
+  },
+  {
+    label: "Correct Grammar",
+    value: "Correct the Grammar from",
+  },
+  {
+    label: "Correct Spelling",
+    value: "Correct the Spelling from",
+  },
+  {
+    label: "Correct Grammar & Spelling",
+    value: "Correct the Grammar and Spelling from",
+  },
+  {
+    label: "Find conclusion",
+    value: "Find the conclusion of",
+  },
+  {
+    label: "Find keyword",
+    value: "Find the keyword of",
+  },
+  {
+    label: "Improvement Suggestion",
+    value: "Give improvment suggestions of",
+  },
+  {
+    label: "Paraphrase",
+    value: "Paraphrase",
+  },
+  {
+    label: "Proofread",
+    value: "Proofread",
+  },
+  {
+    label: "Simplify this text",
+    value: "Simplify",
+  },
+  {
+    label: "Summarize this text",
+    value: "Summarize",
+  },
+  {
+    label: "Rewrite this text",
+    value: "Rewrite",
+  },
+  {
+    label: "Review this text",
+    value: "Review",
   },
 ];
---
+++ src/pages/premium/checkbot.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { FaPlusCircle } from "react-icons/fa";
+import Layout from "@/common/components/Layout";
+import PremiumCheckBotForm from "@/modules/premium/components/CheckbotForm";
+import CheckbotComparison from "@/modules/checkbot/components/CheckbotComparison";
+import MetaSEO from "@/common/components/MetaSEO";
+
+const CheckBot = () => {
+  const [checkbotVal, setCheckbotVal] = useState("");
+  const [showLogin, setShowLogin] = useState(false);
+
+  const seo = {
+    title: "Free Grammar Checker - LanguageAI Checkbot",
+    description:
+      "Grammar check for free! Copy and paste your text in grammar checker. Fix grammar, spelling, and punctuation errors instantly with our cutting-edge AI technology. No sign-up required!",
+    url: `${process.env.NEXT_PUBLIC_BASE_URL}checkbot/`,
+  };
+
+  return (
+    <Layout>
+      <MetaSEO seo={seo} />
+      <div className="container mx-auto p-2 lg:p-4 bg-white">
+        <h1
+          className="bg-black text-3xl rounded flex flex-row items-center justify-center mt-2 lg:my-4 lg:w-1/3 lg:mx-auto"
+          id="title"
+        >
+          <FaPlusCircle className="text-5xl mr-2" />
+          <span>
+            LanguageAI Premium
+          </span>
+        </h1>
+        <h2 className="text-black mt-4 text-center text-lg mb-4 italic">
+          #Ultimate Writing Check for All Languages
+        </h2>
+        <div className="lg:grid lg:grid-cols-3 lg:gap-4 mb-8">
+          <PremiumCheckBotForm
+            dispatchLoginForm={() => {}}
+            dispatchCheckbotVal={(val) => { }}
+          />
+        </div>
+        <div className="text-black">
+          <CheckbotComparison />
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default CheckBot;
