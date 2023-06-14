4ab93e3828d40e9e6aa1e5036d5e4ea4ed1ed1af
stan <winatastanley355@gmail.com>
2023-06-14 17:10:20 +0700

    Update home copywrite

--- src/modules/home/components/HomeCopywriting.tsx
+++ src/modules/home/components/HomeCopywriting.tsx
@@ -1,11 +1,7 @@
 import React from "react";
-import {
-  FaLanguage,
-  FaRobot,
-  FaPlusCircle,
-  FaPlusSquare,
-} from "react-icons/fa";
+import { FaCheck } from "react-icons/fa";
 import Button from "@/common/components/Button";
+import { HOME_COPYWRITING } from "../lib/constant";
 
 const HomeCopywriting = () => {
 
@@ -23,8 +19,26 @@ const HomeCopywriting = () => {
       <div className="text-xl lg:text-3xl text-center my-4 hidden lg:block">
         European, African, or Asian languages are all possible!
       </div>
-      <div className="grid grid-cols-1" >
-        <TranslateCopywriting />
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 mt-8" >
+        {HOME_COPYWRITING.map((copy: any) =>
+          <div key={copy.title} className="flex flex-col items-center">
+            <div className="text-8xl mb-4">{copy.icon}</div>
+            <div className="text-2xl font-semibold">{copy.title}</div>
+            <ul className="text-xl mt-4">
+              {copy.features.map((feature: string) =>
+                <li className="flex items-center gap-2 py-2">
+                  <FaCheck />
+                  <span>{feature}</span>
+                </li>)}
+            </ul>
+            <Button
+              type="link"
+              href={copy.link}
+              title={copy.ctaText}
+              wrapperClassName="bg-white p-2 text-black w-4/5 text-center font-semibold rounded mt-4"
+            />
+          </div>
+        )}
       </div>
     </div>
   )
---
+++ src/modules/home/lib/constant.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import {
+  FaLanguage,
+  FaRobot,
+  FaPlusCircle,
+} from "react-icons/fa";
+
+export const HOME_COPYWRITING = [
+  {
+    icon: <FaLanguage />,
+    title: "LanguageAI Translate",
+    link: "/translate/",
+    ctaText: "Translate",
+    features: [
+      "5x Google Translate Quality",
+      "All Languages Support",
+      "Natural Language Detection",
+      "Context Support",
+      "2000 words max"
+    ]
+  },
+  {
+    icon: <FaPlusCircle />,
+    title: "Premium Translate",
+    link: "/translate/",
+    ctaText: "Translate",
+    features: [
+      "10x Google Translate Quality",
+      "All Languages Support",
+      "Natural Language Detection",
+      "Context Support",
+      "8000 words max"
+    ]
+  },
+  {
+    icon: <FaRobot />,
+    title: "LanguageAI Checkbot",
+    link: "/checkbot/",
+    ctaText: "Let's Check",
+    features: [
+      "10x Grammarly Quality",
+      "All Languages Support",
+      "Natural Language Detection",
+      "Context Support",
+      "Proofread, Rewrite, or Paraphrase",
+      "2000 words max"
+    ]
+  },
+  {
+    icon: <FaPlusCircle />,
+    title: "Premium Checkbot",
+    link: "/premium/checkbot/",
+    ctaText: "Let's Check",
+    features: [
+      "20x Grammarly Quality",
+      "All Languages Support",
+      "Natural Language Detection",
+      "Context Support",
+      "Proofread, Rewrite, or Paraphrase",
+      "8000 words max"
+    ]
+  },
+]
