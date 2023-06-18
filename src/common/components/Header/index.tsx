55b8e5dcb29c6722a1212e938440c782f7375463
stan <winatastanley355@gmail.com>
2023-06-18 11:16:32 +0700

    Header refac

--- src/common/components/Header/index.tsx
+++ src/common/components/Header/index.tsx
@@ -1,16 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Inter } from "next/font/google";
+import { FaAngleRight } from "react-icons/fa";
 import { SiTaichilang } from "react-icons/si";
 import cookie from "js-cookie";
 import classNames from "classnames";
+import { HEADER_MENU } from "./constant";
 import Button from "../Button";
-import MobileHeaderMenu from "./MobileHeaderMenu";
-import DesktopHeaderMenu from "./DesktopHeaderMenu";
 import { useDesktopScreen } from "@/common/hooks/useDesktopScreen";
-
-export interface IHeaderMenu {
-  token: string;
-}
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,7 +28,7 @@ const Header = () => {
   return (
     <nav
       className={classNames(
-        "bg-black w-full p-4 flex flex-row items-center justify-between fixed lg:relative z-10",
+        "bg-black w-full p-4 lg:py-2 flex flex-row items-center justify-between fixed lg:relative z-10",
         inter.className
       )}
     >
@@ -45,19 +41,30 @@ const Header = () => {
         <SiTaichilang className="mr-2" />
         <span>LanguageAI</span>
       </Button>
-
-      {isDesktop ? (
-        <DesktopHeaderMenu token={token} />
-      ) : (
-        <Button
-          type="button"
-          title={showModal ? "Close" : "Menu"}
-          ariaLabel="header_menu_btn"
-          onClick={() => setShowModal(!showModal)}
-          wrapperClassName="text-md active:underline"
-        />
-      )}
-      {showModal && <MobileHeaderMenu token={token} />}
+      <Button
+        type="button"
+        title={showModal ? "Close" : "Menu"}
+        ariaLabel="header_menu_btn"
+        onClick={() => setShowModal(!showModal)}
+        wrapperClassName="text-md active:underline lg:hidden"
+      />
+      {(showModal || isDesktop) && <div className="bg-black absolute lg:static lg:flex lg:gap-4 top-16 left-0 text-lg lg:text-xl w-full lg:w-fit h-screen lg:h-fit px-2 lg:px-0">
+        {HEADER_MENU
+          .filter(menu =>
+            token ? menu.title !== "Login" : menu.title !== "Profile"
+          ).map((menu, i) =>
+            <Button
+              type="link"
+              href={menu.url}
+              buttonClassName="flex items-center gap-2"
+              wrapperClassName={classNames("border-b lg:border-b-0 p-2 lg:p-0 hover:border-b", i === 0 ? "border-t lg:border-t-0" : "")}
+            >
+              {menu.icon}
+              <span>{menu.title}</span>
+              <FaAngleRight className="ml-auto lg:hidden" />
+            </Button>
+          )}
+      </div>}
     </nav>
   );
 };
