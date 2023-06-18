9bd5e542118cfae2fceb41a40dfce1ce3df98f8f
stan <winatastanley355@gmail.com>
2023-06-18 14:37:41 +0700

    update font size

--- src/modules/home/components/HomeHero.tsx
+++ src/modules/home/components/HomeHero.tsx
@@ -5,11 +5,11 @@ import styles from './home.module.scss';
 
 const HomeHero = () => (
   <div className={classNames(styles.home__hero, "p-2 lg:px-4 h-screen")}>
-    <div className="mt-80 text-6xl mb-4 font-semibold">
+    <div className="mt-80 text-6xl lg:text-8xl mb-4 font-semibold">
       <h1 className="mb-4">LanguageAI</h1>
       <h2>v{PackageJson.version}</h2>
     </div>
-    <div className="text-2xl font-semibold">
+    <div className="text-2xl lg:text-3xl font-semibold">
       AI Translation and Text Analysis for All Languages
     </div>
   </div>
