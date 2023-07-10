467ceff62e91d42759eeab13dac246b15571c66c
stan <winatastanley355@gmail.com>
2023-07-10 15:14:19 +0700

    Save history func

--- src/common/lib/saveHistory.ts
+++ src/common/lib/saveHistory.ts
@@ -1,6 +1,4 @@
-import getConfig from "next/config";
-import { createClient } from "redis";
-
+import axios from "axios";
 
 interface IRedisHistoryValue {
   time: any;
@@ -14,5 +12,17 @@ export const saveHistory = async (
   userID: string,
   history: IRedisHistoryValue
 ) => {
-  
+  const URL = `${process.env.NEXT_PUBLIC_BASE_URL}api/redis/history/`;
+  const saveHistoryPayload = {
+    userID,
+    history,
+  };
+  const axiosConfig = {
+    method: "POST",
+    url: URL,
+    data: saveHistoryPayload,
+  };
+
+  const { data } = await axios(axiosConfig);
+  return data;
 };
