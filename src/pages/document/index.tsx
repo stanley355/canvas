a836fd5567b67b548101f84a215647fdd1fe0505
stan <winatastanley355@gmail.com>
2023-07-16 14:35:01 +0700

    Document page

---
+++ src/pages/document/index.tsx
@@ -0,0 +1,14 @@
+import Layout from '@/common/components/Layout';
+import React from 'react';
+
+const Document = () => {
+  return (
+    <Layout>
+      <div className='container mx-auto bg-white text-black p-4 h-screen'>
+        <h1 className='text-xl font-bold'>My Document</h1>
+      </div>
+    </Layout>
+  );
+};
+
+export default Document;
