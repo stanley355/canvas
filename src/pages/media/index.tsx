40bce6c1cfdc28f47f76cc14e3082b5062468ea7
stan <winatastanley355@gmail.com>
2023-07-27 11:09:37 +0700

    Media design update

--- src/pages/media/index.tsx
+++ src/pages/media/index.tsx
@@ -7,6 +7,8 @@ import Layout from "@/common/components/Layout";
 import { getMediaHomeData } from "@/modules/media/lib/getMediaHomeData";
 import MetaSEO from "@/common/components/MetaSEO";
 import { HOME_SEO } from "@/modules/home/lib/constant";
+import MediaHomeChildArticles from "@/modules/media/components/MediaHomeChildArticles";
+import MediaHomeSideArticles from "@/modules/media/components/MediaHomeSideArticles";
 
 interface IMediaHome {
   articles: Array<any>;
@@ -18,91 +20,44 @@ const MediaSlug = (props: IMediaHome) => {
   return (
     <Layout>
       <MetaSEO seo={HOME_SEO} />
-      <div className="container bg-white mx-auto p-4 text-black min-h-screen">
-        <div className="font-semibold text-2xl text-center my-4 flex items-center gap-2 justify-center">
-          <SiTaichilang />
-          <span>LanguageAI NewsLetter</span>
-        </div>
-        <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 mb-8">
-          {articles.length > 0 &&
-            articles.slice(0, 4).map((article: any) => (
-              <div key={article.id} className="h-80 border rounded-lg bg-gradient-to-b from-white via-blue-100 to-white shadow-lg shadow-slate-400">
-                <div>
-                  <img
-                    src={article?.heroImg?.url}
-                    alt={article?.heroImg?.alt}
-                    loading="lazy"
-                    className="rounded-md w-full h-auto max-h-40"
-                    width={400}
-                    height={400}
-                  />
-                </div>
-                <div className="p-2">
-                  <Link href={`/media/${article.slug}/`}>
-                    <div className=" hover:text-blue-900 text-xl hover:underline pt-2">
-                      {article.title}
-                    </div>
-                  </Link>
-                  <div className="pb-6">
-                    {article?._publishedAt &&
-                      new Date(article?._publishedAt).toLocaleString()}
-                  </div>
-                </div>
-              </div>
-            ))}
-        </div>
-        <div className="grid grid-cols-1 lg:grid-cols-3 lg:gap-4">
-          <div className="lg:col-span-2">
+      <div className="bg-gradient-to-br from-white via-blue-300 to-white">
+
+        <div className="container bg-transparent mx-auto p-4 text-black min-h-screen">
+          <div className="font-semibold text-2xl text-center my-4 flex items-center gap-2 justify-center">
+            <SiTaichilang />
+            <span>LanguageAI NewsLetter</span>
+          </div>
+          <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 mb-8">
             {articles.length > 0 &&
-              articles.slice(4, Math.floor(articles.length / 2)).map((article: any) => (
-                <div key={article.id} className="flex flex-row border mb-8 rounded-lg bg-gradient-to-b from-white via-blue-100 to-white shadow-lg shadow-slate-400">
-                  <div className="w-1/2 lg:w-full">
+              articles.slice(0, 4).map((article: any) => (
+                <div key={article.id} className="h-80 border rounded-lg bg-gradient-to-b from-white via-blue-100 to-white shadow-lg shadow-slate-400">
+                  <div>
                     <img
                       src={article?.heroImg?.url}
                       alt={article?.heroImg?.alt}
                       loading="lazy"
-                      className="rounded-md w-full lg h-full"
+                      className="rounded-md w-full h-auto max-h-40"
                       width={400}
                       height={400}
                     />
                   </div>
                   <div className="p-2">
                     <Link href={`/media/${article.slug}/`}>
-                      <div className=" hover:text-blue-900 text-lg hover:underline mb-2">
+                      <div className=" hover:text-blue-900 text-xl hover:underline pt-2">
                         {article.title}
                       </div>
                     </Link>
-                    <div >
+                    <div className="pb-6">
                       {article?._publishedAt &&
                         new Date(article?._publishedAt).toLocaleString()}
-                    </div>
-                    <div className="text-sm hidden lg:block">
-                      {article.metaTags.description}
                     </div>
                   </div>
                 </div>
               ))}
           </div>
-          <div>
-            {articles.length > 0 &&
-              articles.slice(articles.length / 2, articles.length - (articles.length / 4)).map((article: any) => (
-                <div key={article.id} className="flex flex-row border mb-8 rounded-lg bg-gradient-to-b from-white via-blue-100 to-white shadow-lg shadow-slate-400">
-                  <div className="p-2">
-                    <Link href={`/media/${article.slug}/`}>
-                      <div className=" hover:text-blue-900 text-lg underline mb-2">
-                        {article.title}
-                      </div>
-                    </Link>
-                    <div >
-                      {article?._publishedAt &&
-                        new Date(article?._publishedAt).toLocaleString()}
-                    </div>
-                    <div className="text-sm ">
-                      {article.metaTags.description}
-                    </div>
-                  </div>
-                </div>
-              ))}
+          <div className="grid grid-cols-1 lg:grid-cols-3 lg:gap-4">
+            <MediaHomeChildArticles articles={articles} />
+            <MediaHomeSideArticles articles={articles} />
           </div>
         </div>
       </div>
