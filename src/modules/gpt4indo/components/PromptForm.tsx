24c40578acefb3271e9acaab13ff7fdd826292e6
stan <winatastanley355@gmail.com>
2023-07-28 13:19:56 +0700

    Handleprompt

--- src/modules/gpt4indo/components/PromptForm.tsx
+++ src/modules/gpt4indo/components/PromptForm.tsx
@@ -1,16 +1,53 @@
-import React from 'react';
-import { FaChevronCircleRight } from 'react-icons/fa';
+import React, { useState } from 'react';
+import { FaChevronCircleRight, FaSpinner } from 'react-icons/fa';
 import Button from '@/common/components/Button';
+import { toast } from 'react-toastify';
+import { handlePrompt } from '@/common/lib/handlePrompt';
 
-const PromptForm = () => {
+interface IPromptForm {
+  dispatchPrompt: (prompt: string) => void;
+  dispatchCompletion: (prompt: string, completion: string) => void;
+}
+
+const PromptForm = (props: IPromptForm) => {
+  const { dispatchPrompt, dispatchCompletion } = props;
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const target = e.target as any;
+    const prompt = target.prompt.value;
+
+    if (!prompt) {
+      toast.warning("Instruksi Anda belum diisi!");
+      return;
+    }
+
+    dispatchPrompt(prompt);
+    setIsLoading(true);
+    const { content } = await handlePrompt(prompt)
+    if (content) {
+      dispatchCompletion(prompt, content);
+      setIsLoading(false);
+      return;
+    }
+
+    setIsLoading(false);
+    dispatchCompletion(prompt, "Terjadi kesalahan, silakan coba lagi");
+    return;
+  }
+
+
+
   return (
-    <form className='bg-gray-700 absolute left-0 bottom-0 w-full p-2 pb-4' onSubmit={(e: React.FormEvent) => e.preventDefault()}>
+    <form className='bg-gray-700 absolute left-0 bottom-0 w-full p-2 pb-4 text-black' onSubmit={handleSubmit}>
       <div className='w-full lg:w-2/3 rounded-md bg-white flex items-center lg:mx-auto'>
-        <label htmlFor="" className='rounded-t rounded-lg w-[90%] lg:w-[95%]'>
-          <textarea name="" id="" placeholder='Masukkan Instruksi Anda' className='w-full rounded p-1 bg-transparent outline-none resize-none overflow-y-auto' />
+        <label htmlFor="prompt_text" className='rounded-t rounded-lg w-[90%] lg:w-[95%]'>
+          <textarea name="prompt" id="prompt_text" placeholder='Masukkan Instruksi Anda' className='w-full rounded p-1 bg-transparent outline-none resize-none overflow-y-auto' />
         </label>
-        <Button type='submit' wrapperClassName='w-[10%] lg:w-[5%]' buttonClassName='w-full h-full'>
-          <FaChevronCircleRight className='mx-auto text-2xl' />
+        <Button type='submit' wrapperClassName='w-[10%] lg:w-[5%]' buttonClassName='w-full h-full' disabled={isLoading}>
+          {isLoading ? <FaSpinner className='mx-auto text-2xl animate-spin' /> :
+            <FaChevronCircleRight className='mx-auto text-2xl' />}
         </Button>
       </div>
     </form>
--- src/pages/gpt4indo.tsx
+++ src/pages/gpt4indo.tsx
@@ -1,36 +1,64 @@
-import Button from '@/common/components/Button';
-import PromptForm from '@/modules/gpt4indo/components/PromptForm';
-import React from 'react';
-import { FaArrowCircleRight, FaChevronCircleRight } from 'react-icons/fa';
+import React, { useState } from 'react';
 import { SiOpenai } from 'react-icons/si';
+import PromptForm from '@/modules/gpt4indo/components/PromptForm';
+import SystemIntro from '@/modules/gpt4indo/components/SystemIntro';
+import { FaSpinner, FaUserCircle } from 'react-icons/fa';
+
+interface IChat {
+  prompt: string;
+  completion: string;
+}
 
 const GPT4Indo = () => {
+  const [chats, setChats] = useState<Array<IChat>>([]);
 
-  const SystemIntro = () => (
-    <div className='flex items-center gap-2 bg-gray-700 p-2 text-white lg:justify-center'>
-    <div className='text-xl'>
-      <SiOpenai />
-    </div>
-    <div>
-      Halo, bagaimana saya bisa membantu Anda?
-    </div>
-  </div>
-  );
+  const handleNewPrompt = (prompt: string) => {
+    let newChats: any = structuredClone(chats);
+    newChats.push({ prompt, completion: "" });
+    setChats(newChats);
+  }
+
+  // console.log(chats);
+  const handleNewCompletion = (prompt: string, completion: string) => {
+    let newChats: any = structuredClone(chats);
+    newChats.push({ prompt, completion });
+    setChats(newChats);
+    return;
+  }
 
   return (
     <div className='grid grid-cols-1 lg:grid-cols-5'>
       <div className='h-screen lg:col-span-1 border hidden lg:block'>
         hi
       </div>
-      <div className='h-screen bg-white text-black lg:col-span-4 relative'>
+      <div className='h-screen bg-white: lg:col-span-4 relative'>
         <div className='text-center bg-black p-2 text-white lg:hidden'>
           GPT4Indo
         </div>
         <div className='h-[95%]'>
-          <div className='h-[90%] overflow-y-auto'>
+          <div className='h-[91%] lg:h-[96%] overflow-y-auto bg-gray-700'>
             <SystemIntro />
+            {chats.length > 0 && chats.map((chat: any, index) =>
+              <div key={index}>
+                <div className='bg-gray-800 p-4 text-white border-b'>
+                  <div className='flex items-center gap-2 lg:w-2/3 lg:mx-auto'>
+                    <div className='text-xl'><FaUserCircle /></div>
+                    <div>{chat.prompt}</div>
+                  </div>
+                </div>
+                <div className='bg-gray-700 p-4 text-white border-b'>
+                  <div className='flex items-center gap-2 lg:w-2/3 lg:mx-auto'>
+                    <div className='text-xl'><SiOpenai /></div>
+                    <div>{chat.completion ? chat.completion : <FaSpinner className='animate-spin' />}</div>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
-          <PromptForm />
+          <PromptForm
+            dispatchPrompt={handleNewPrompt}
+            dispatchCompletion={handleNewCompletion}
+          />
         </div>
       </div>
     </div>
