4e448af7804ba2c21139f99489363719cb2158cb
stan <winatastanley355@gmail.com>
2023-07-28 14:23:46 +0700

    Update

--- src/modules/gpt4indo/components/GPT4IndoSidebar.tsx
+++ src/modules/gpt4indo/components/GPT4IndoSidebar.tsx
@@ -8,7 +8,7 @@ const GPT4IndoSidebar = () => {
   return (
     <div className="h-screen lg:col-span-1 hidden lg:block">
       <div className="text-center p-2 pb-0 text-2xl">GPT4Indo</div>
-      <div className="text-center">Subsidiary of LanguageAI</div>
+      <div className="text-center">Beta Access from LanguageAI</div>
 
       <div className="mt-16">
         <div className="text-center mb-2">Explore Other Stuffs</div>
