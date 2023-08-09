966345908f95347f124f4d2f4b72caec4e8198f9
stan <winatastanley355@gmail.com>
2023-08-09 13:22:32 +0700

    udpate logo

--- src/modules/home/components/HomeHero.tsx
+++ src/modules/home/components/HomeHero.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import classNames from "classnames";
+import Image from "next/image";
 
 import { HEADER_MENU } from "@/common/components/Header/constant";
 import Button from "@/common/components/Button";
@@ -10,12 +11,15 @@ const HomeHero = () => (
       "bg-gradient-to-b from-black via-blue-900 to-white lg:h-screen p-4 pb-8"
     )}
   >
-    <h1 className="text-center text-4xl mb-4 lg:m-8 lg:text-6xl">LanguageAI</h1>
+    <h1 className="text-center flex items-center justify-center text-4xl">
+      <span>Language</span>
+      <Image src="/images/languageai_white.png" alt="Language AI" width={50} height={50} />
+    </h1>
     <div className="text-2xl text-center">
-      AI Translation and Text Analysis for All Languages
+      Language made Eazy
     </div>
     <div className="mt-12 lg:hidden">
-      {HEADER_MENU.slice(0, 4).map((menu:any, i: number) => (
+      {HEADER_MENU.slice(0, 4).map((menu: any, i: number) => (
         <Button
           type="link"
           href={menu.url}
@@ -32,7 +36,7 @@ const HomeHero = () => (
       ))}
     </div>
     <div className="mt-12 hidden w-1/2 mx-auto lg:grid grid-cols-2 gap-4">
-      {HEADER_MENU.slice(0, 4).map((menu:any) => (
+      {HEADER_MENU.slice(0, 4).map((menu: any) => (
         <div
           key={menu.title}
           className="bg-white rounded-md text-black p-2 group"
