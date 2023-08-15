2328a9594be1b75f2b6a5d7d25bdc0a9ba098d46
stan <winatastanley355@gmail.com>
2023-08-15 15:16:04 +0700

    language gpt

--- src/modules/home/components/HomeHero.tsx
+++ src/modules/home/components/HomeHero.tsx
@@ -2,19 +2,17 @@ import React from "react";
 import classNames from "classnames";
 import Link from "next/link";
 import Image from "next/image";
-
-import Button from "@/common/components/Button";
 import Router from "next/router";
+import Button from "@/common/components/Button";
 
 const HomeHero = () => {
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-
     const target = e.target as any;
     const prompt = target.prompt.value;
-
     Router.push(`/languagegpt?prompt=${prompt}`);
-  }
+  };
+
   return (
     <div
       className={classNames(
@@ -41,7 +39,7 @@ const HomeHero = () => {
           />
         </label>
 
-        <Button type='submit' wrapperClassName='h-full text-white text-lg w-full bg-black p-1 mt-4 lg:w-1/2 lg:mx-auto' buttonClassName='w-full h-full flex items-center justify-center'>
+        <Button type='submit' wrapperClassName='h-full text-white text-lg w-full bg-black p-1 mt-4 lg:w-1/2 lg:mx-auto border-white border' buttonClassName='w-full h-full flex items-center justify-center'>
           <span>Go</span>
         </Button>
       </form>
@@ -49,10 +47,10 @@ const HomeHero = () => {
       <div className="mt-8 lg:w-2/3 lg:mx-auto">
         <div className="text-center text-lg font-semibold mb-4">You might want to try</div>
         <div className="grid grid-cols-2 gap-4">
-          <Link href="/languagegpt?prompt=Translate 'Apa kabar?' in German, English, and Japanese" className="bg-white text-black p-1 rounded">Translate &quot;Apa kabar?&quot; in German, English, and Japanese</Link>
-          <Link href="/languagegpt?prompt=What is the meaning of 'Als ik Netherlander was'?" className="bg-white text-black p-1 rounded">What is the meaning of &quot;Als ik Netherlander was&quot;?</Link>
-          <Link href="/languagegpt?prompt=Fix my typo: There re ten thosand weys to go to Rome" className="bg-white text-black p-1 rounded">Fix my typo: There re ten thosand weys to go to Rome </Link>
-          <Link href="/languagegpt?prompt=What is the hardest language in the world?" className="bg-white text-black p-1 rounded">What is the hardest language in the world?</Link>
+          <Link href="/languagegpt?prompt=Translate 'Apa kabar?' in German, English, and Japanese" className="bg-white text-black p-2 rounded">Translate &quot;Apa kabar?&quot; in German, English, and Japanese</Link>
+          <Link href="/languagegpt?prompt=What is the meaning of 'Als ik Netherlander was'?" className="bg-white text-black p-2 rounded">What is the meaning of &quot;Als ik Netherlander was&quot;?</Link>
+          <Link href="/languagegpt?prompt=Fix my spelling: There re ten thosand weys to go to Rome" className="bg-white text-black p-2 rounded">Fix my spelling: There re ten thosand weys to go to Rome </Link>
+          <Link href="/languagegpt?prompt=What is the hardest language in the world?" className="bg-white text-black p-2 rounded">What is the hardest language in the world?</Link>
         </div>
       </div>
     </div>
--- src/pages/languagegpt.tsx
+++ src/pages/languagegpt.tsx
@@ -1,13 +1,15 @@
+import React, { useEffect, useReducer } from 'react';
+import dynamic from 'next/dynamic';
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
+
+import MetaSEO from '@/common/components/MetaSEO';
 import Layout from '@/common/components/Layout';
-import { languageGPTReducer } from '@/modules/languagegpt/lib/reducer';
-import { } from '@/modules/checkbot/lib/reducer';
 import CompletionBox from '@/modules/languagegpt/components/CompletionBox';
 import PromptForm from '@/modules/languagegpt/components/PromptForm';
+import { languageGPTReducer } from '@/modules/languagegpt/lib/reducer';
 import { LANGUAGE_GPT_STATES } from '@/modules/languagegpt/lib/states';
-import React, { useReducer } from 'react';
-import MetaSEO from '@/common/components/MetaSEO';
 import { HOME_SEO } from '@/modules/home/lib/constant';
-import dynamic from 'next/dynamic';
+import { handlePrompt } from '@/common/lib/handlePrompt';
 
 const LoginModal = dynamic(
   () => import("../modules/login/components/LoginModal")
@@ -17,13 +19,20 @@ const NoPlansModal = dynamic(
   () => import("../modules/premium/components/NoPlansModal")
 );
 
-const LanguageGPT = () => {
+const LanguageGPT = (props: any) => {
+  const { queryPromptAndCompletionList } = props;
   const [state, dispatch] = useReducer(languageGPTReducer, LANGUAGE_GPT_STATES);
   const { showLogin, showBalanceModal, promptAndCompletionList } = state;
 
   const updateState = (name: string, value: any) => {
     dispatch({ type: "UPDATE", name, value });
   };
+
+  useEffect(() => {
+    if (queryPromptAndCompletionList.length > 0) {
+      updateState("promptAndCompletionList", queryPromptAndCompletionList);
+    }
+  }, [queryPromptAndCompletionList]);
 
   return (
     <Layout>
@@ -39,3 +48,38 @@ const LanguageGPT = () => {
 };
 
 export default LanguageGPT;
+export const getServerSideProps: GetServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  const query = ctx.query;
+  const token = ctx.req.cookies.token;
+
+  if (!token) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/login/",
+      },
+    };
+  }
+
+  if (query?.prompt) {
+    const { content } = await handlePrompt(String(query?.prompt));
+
+    if (content) {
+      const queryPromptAndCompletionList = [
+        { role: "user", prompt: query.prompt },
+        { role: "system", prompt: content }
+      ]
+      return {
+        props: {
+          queryPromptAndCompletionList
+        }
+      }
+    }
+  }
+
+  return {
+    props: {
+      queryPromptAndCompletionList: []
+    }
+  }
+}
