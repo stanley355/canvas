4123210004dc7a13af600b3c49e955696222ee45
stan <winatastanley355@gmail.com>
2023-09-09 10:41:32 +0700

    Redirect all premium to free

--- src/pages/premium/checkbot.tsx
+++ src/pages/premium/checkbot.tsx
@@ -1,82 +1,28 @@
-import React, { useReducer } from "react";
-import { FaClock, FaRobot } from "react-icons/fa";
-import dynamic from "next/dynamic";
+import React from "react";
+import Link from "next/link";
+import { GetServerSideProps } from "next";
 
 import Layout from "@/common/components/Layout";
-import MetaSEO from "@/common/components/MetaSEO";
-import Button from "@/common/components/Button";
-import PremiumCheckbotArea from "@/modules/premium/components/PremiumCheckbotArea";
-import HistoryBar from "@/common/components/HistoryBar";
-
-import { PREMIUM_CHECKBOT_SEO } from "@/modules/premium/lib/constant";
-import { CHECKBOT_STATES } from "@/modules/checkbot/lib/states";
-import { checkbotReducer } from "@/modules/checkbot/lib/reducer";
-import { sendFirebaseEvent } from "@/common/lib/firebase/sendFirebaseEvent";
-import { PlansSection } from "../plans";
-
-const LoginModal = dynamic(
-  () => import("../../modules/login/components/LoginModal")
-);
-
-const NoPlansModal = dynamic(
-  () => import("../../modules/premium/components/NoPlansModal")
-);
 
 const CheckBot = () => {
-  const [states, dispatch] = useReducer(checkbotReducer, CHECKBOT_STATES);
-  const { showLogin, showBalanceModal, showHistory } = states;
-
-  const updateState = (name: string, value: any) => {
-    dispatch({ type: "UPDATE", name, value });
-  };
-
-  const handleHistoryClick = (history: any) => {
-    updateState("originalText", history.originalText);
-    updateState("checkbotCompletion", history.completionText);
-    updateState("showHistory", false);
-  };
-
   return (
     <Layout>
-      {showLogin && <LoginModal />}
-      {showBalanceModal && <NoPlansModal />}
-      <MetaSEO seo={PREMIUM_CHECKBOT_SEO} />
-      <div className="bg-gradient-to-b from-white via-slate-400 to-white pb-6">
-        <div className="container mx-auto p-2 lg:px-2">
-          <h1
-            className="text-black my-4 text-3xl rounded flex items-center justify-center font-semibold lg:w-1/3 lg:mx-auto"
-            id="title"
-          >
-            <FaRobot className="text-3xl mr-2" />
-            <span>Checkbot+</span>
-          </h1>
-          <PremiumCheckbotArea states={states} updateState={updateState} />
-          <Button
-            type="button"
-            wrapperClassName="p-2 w-fit bg-blue-900 rounded-md mx-auto cursor-pointer mb-8"
-            buttonClassName="w-full flex items-center gap-2 h-full"
-            onClick={() => {
-              sendFirebaseEvent("show_history", {});
-              updateState("showHistory", !showHistory);
-            }}
-          >
-            <FaClock />
-            <span>Show History</span>
-          </Button>
-          {showHistory && (
-            <HistoryBar
-              pageType="checkbot"
-              onHistoryClick={handleHistoryClick}
-              onCloseClick={() => updateState("showHistory", false)}
-            />
-          )}
-        </div>
-      </div>
-      <div className="bg-white">
-        <PlansSection />
+      <div className="container mx-auto p-4 h-screen flex flex-col items-center border border-white">
+        <h1 className="text-3xl mb-8">This page has been moved</h1>
+        <Link href="/translate" className="w-fit mx-auto bg-white text-black p-4 rounded-lg">Click here to redirect</Link>
       </div>
     </Layout>
   );
 };
 
 export default CheckBot;
+export const getServerSideProps: GetServerSideProps = async () => {
+
+  return {
+    redirect: {
+      permanent: true,
+      destination: "/checkbot/",
+    },
+  };
+};
+
--- src/pages/premium/translate.tsx
+++ src/pages/premium/translate.tsx
@@ -1,125 +1,28 @@
-import React, { useReducer } from "react";
-import { FaClock, FaLanguage } from "react-icons/fa";
-import dynamic from "next/dynamic";
-
-import MetaSEO from "@/common/components/MetaSEO";
-import Button from "@/common/components/Button";
+import React from "react";
 import Layout from "@/common/components/Layout";
-import MediaSelect from "@/common/components/MediaSelect";
-import PremiumTranslateForm from "@/modules/premium/components/TranslateForm";
-import TranslateResult from "@/modules/translate/components/TranslateResult";
-import ImageToTextUploader from "@/common/components/ImageToTextUploader";
-import HistoryBar from "@/common/components/HistoryBar";
+import { GetServerSideProps } from "next";
+import Link from "next/link";
 
-import { sendFirebaseEvent } from "@/common/lib/firebase/sendFirebaseEvent";
-import { PREMIUM_TRANSLATE_SEO } from "@/modules/premium/lib/constant";
-import { premiumTranslateReducer } from "@/modules/premium/lib/reducer";
-import { PREMIUM_TRANSLATE_STATES } from "@/modules/premium/lib/states";
-import { PlansSection } from "../plans";
-
-const LoginModal = dynamic(
-  () => import("../../modules/login/components/LoginModal")
-);
 
 const PremiumTranslate = () => {
-  const [state, dispatch] = useReducer(
-    premiumTranslateReducer,
-    PREMIUM_TRANSLATE_STATES
-  );
-  const {
-    originalText,
-    showHistory,
-    isImageTranslate,
-    imageText,
-    showLogin,
-    translateCompletion,
-  } = state;
-
-  const updateState = (name: string, value: any) => {
-    dispatch({ type: "UPDATE", name, value });
-  };
-
-  const onImageTextDispatch = (txt: string) => {
-    sendFirebaseEvent("image_translate", {});
-    updateState("imageText", txt);
-    updateState("isImageTranslate", false);
-    return;
-  };
-
-  const handleHistoryClick = (history: any) => {
-    updateState("originalText", history.originalText);
-    updateState("translateCompletion", history.completionText);
-    updateState("showHistory", false);
-  };
 
   return (
     <Layout>
-      {showLogin && <LoginModal />}
-      <MetaSEO seo={PREMIUM_TRANSLATE_SEO} />
-      <div className="bg-gradient-to-b from-white via-slate-400 to-white pb-6">
-        <div className="container mx-auto p-2 lg:px-0">
-          <div className="flex items-center gap-4 justify-between text-black">
-            <h1
-              className="text-2xl font-semibold rounded flex items-center justify-center my-4"
-              id="title"
-            >
-              <FaLanguage className="text-4xl mr-2" />
-              <span>Translate+</span>
-            </h1>
-            <MediaSelect
-              onChange={(option) =>
-                updateState("isImageTranslate", option.value === "image")
-              }
-            />
-          </div>
-          <div className="lg:grid lg:grid-cols-2 lg:gap-4 mb-8">
-            {isImageTranslate ? (
-              <div className="mb-2">
-                <ImageToTextUploader
-                  titleColor="black"
-                  dispatch={onImageTextDispatch}
-                />
-              </div>
-            ) : (
-              <PremiumTranslateForm
-                originalText={originalText}
-                imageText={imageText}
-                onReuploadClick={() => updateState("isImageTranslate", true)}
-                dispatchLoginForm={() => updateState("showLogin", true)}
-                dispatchTranslateVal={(trans: string) =>
-                  updateState("translateCompletion", trans)
-                }
-              />
-            )}
-            <TranslateResult translateVal={translateCompletion} />
-          </div>
-
-          <Button
-            type="button"
-            wrapperClassName="p-2 w-fit bg-blue-900 rounded-md mx-auto cursor-pointer mb-8"
-            buttonClassName="w-full flex items-center gap-2 h-full"
-            onClick={() => {
-              sendFirebaseEvent("show_history", {});
-              updateState("showHistory", !showHistory);
-            }}
-          >
-            <FaClock />
-            <span>Show History</span>
-          </Button>
-          {showHistory && (
-            <HistoryBar
-              pageType="translate"
-              onHistoryClick={handleHistoryClick}
-              onCloseClick={() => updateState("showHistory", false)}
-            />
-          )}
-        </div>
-      </div>
-      <div className="bg-white">
-        <PlansSection />
+      <div className="container mx-auto p-4 h-screen flex flex-col items-center border border-white">
+        <h1 className="text-3xl mb-8">This page has been moved</h1>
+        <Link href="/translate" className="w-fit mx-auto bg-white text-black p-4 rounded-lg">Click here to redirect</Link>
       </div>
     </Layout>
   );
 };
 
 export default PremiumTranslate;
+export const getServerSideProps: GetServerSideProps = async () => {
+
+  return {
+    redirect: {
+      permanent: true,
+      destination: "/translate/",
+    },
+  };
+};
