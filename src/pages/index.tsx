8de74a9b32c464aab51fe5f6b7ffee33ae98bb52
Stanley Winata <winatastanley355@gmail.com>
2023-09-23 13:08:20 +0700

    Paid notice

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { FaLanguage, FaRobot } from "react-icons/fa";
 import { HOME_SEO } from "@/modules/home/lib/constant";
 import MetaSEO from "@/common/components/MetaSEO";
 import Layout from "@/common/components/Layout";
+import PaidNotice from "@/common/components/PaidNotice";
 
 const Home = () => {
   return (
