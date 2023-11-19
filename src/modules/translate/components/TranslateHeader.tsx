9362ce54a7283996044ccb23b3f92d1359ad1cea
Stanley Winata <winatastanley355@gmail.com>
2023-11-19 14:52:31 +0700

    Refac translate

--- src/modules/translate/components/TranslateHeader.tsx
+++ src/modules/translate/components/TranslateHeader.tsx
@@ -1,19 +1,15 @@
 import React from "react";
 import { FaLanguage } from "react-icons/fa6";
-import TranslateTypeDropdown from "./TranslateTypeDropdown";
 
 const TranslateHeader = () => {
   return (
-    <div className="flex items-center justify-between text-black px-2">
-      <h1
-        className="text-xl font-semibold rounded flex items-center justify-center my-4"
-        id="title"
-      >
-        <FaLanguage className="text-4xl mr-2" />
-        <span>AI Translate</span>
-      </h1>
-      <TranslateTypeDropdown />
-    </div>
+    <h1
+      className="text-xl font-semibold rounded flex items-center justify-center my-4"
+      id="title"
+    >
+      <FaLanguage className="text-4xl mr-2" />
+      <span>AI Translate</span>
+    </h1>
   );
 };
 
