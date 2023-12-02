3252c810f11b2ff753c6b413ad09eed16cc44db1
Stanley Winata <winatastanley355@gmail.com>
2023-12-02 14:13:14 +0700

    Remove unused code

--- src/common/components/Layout.tsx
+++ src/common/components/Layout.tsx
@@ -1,28 +1,14 @@
-import React, { useEffect, useState } from "react";
 import { ToastContainer } from "react-toastify";
-import classNames from "classnames";
 import Header from "./Header";
 import Footer from "./Footer";
 import { Inter } from "next/font/google";
-import "react-toastify/dist/ReactToastify.css";
-import PaidNotice from "./PaidNotice";
 import Cookies from "js-cookie";
+import "react-toastify/dist/ReactToastify.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const token = Cookies.get("paid_notice");
-  const [showPaidNotice, setShowPaidNotice] = useState(false);
-
-  useEffect(() => {
-    if (!token) {
-      setShowPaidNotice(true);
-    }
-
-    () => {
-      setShowPaidNotice(false);
-    };
-  }, [token]);
 
   return (
     <div className={inter.className}>
