6f84935b8f12e40c6992d99642f702b6931d8c62
Stanley Winata <winatastanley355@gmail.com>
2023-12-09 17:57:37 +0700

    No plan modal

--- src/modules/plans/components/NoPlansModal.tsx
+++ src/modules/plans/components/NoPlansModal.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import { FaExclamationCircle} from "react-icons/fa";
+import { FaExclamationCircle } from "react-icons/fa";
 import Link from "next/link";
 
 const NoPlansModal = () => {
@@ -19,13 +19,18 @@ const NoPlansModal = () => {
 
         <div className="flex flex-col items-center justify-center mt-4">
           <FaExclamationCircle className="text-blue-900 text-5xl my-4" />
-          <div className="text-xl text-center font-bold">You have no Ongoing Subscription</div>
-          <div className="text-center">
-            Subscribe our Platform to continue, start with only{" "}
-            <span className="font-bold">Rp1</span>
+          <div className="text-xl text-center font-bold">
+            Anda tidak ada paket berlangganan
           </div>
-          <Link href="/plans/" className="py-2 bg-blue-900 mt-4 w-full text-white font-bold text-center rounded-lg">
-          Subscribe 
+          <div className="text-center text-lg">
+            Yuk berlangganan, harga mulai dari
+            <span className="font-bold"> Rp1</span> aja loh
+          </div>
+          <Link
+            href="/plans/"
+            className="py-2 bg-blue-900 mt-4 w-full text-white font-bold text-center rounded-lg"
+          >
+            Mulai Berlangganan
           </Link>
         </div>
       </div>
