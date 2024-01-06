365b77158a22cc7f9d9ed205068bd39949b5626b
Stanley Winata <winatastanley355@gmail.com>
2024-01-06 09:26:52 +0700

    Translate seo update

--- src/modules/translate/lib/constant.ts
+++ src/modules/translate/lib/constant.ts
@@ -1,8 +1,7 @@
 export const TRANSLATE_SEO = {
-  title:
-    "Language AI: Terjemahan akurat dari bahasa Indonesia ke Inggris",
-  description: `Terjemahan teks & berkas secara akurat dari bahasa Indonesia ke Inggris 
-  dan lebih dari 100 bahasa lainnya dengan penerjemah akurat Language AI`,
-  keywords: `translate, terjemahan, penerjemah, bahasa, indonesia, Inggris`,
+  title: "Language AI Translate: Penerjemah paling akurat di seluruh dunia",
+  description:
+    "Layanan AI Translate yang ditawarkan tanpa biaya ini dapat langsung menerjemahkan berbagai kata dan frasa ke bahasa Indonesia dan lebih dari 100 bahasa lainnya.",
+  keywords: `translate, terjemahkan, terjemahan, penerjemahan, penerjemah, terjemahan mesin, terjemahan online`,
   url: `${process.env.NEXT_PUBLIC_BASE_URL}/translate`,
 };
