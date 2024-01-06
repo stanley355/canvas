bc02f1c5e288977dd5c1a2b3a11a8eb18e784a20
Stanley Winata <winatastanley355@gmail.com>
2024-01-06 09:44:07 +0700

    Update seo

--- src/modules/checkbot/lib/constant.ts
+++ src/modules/checkbot/lib/constant.ts
@@ -1,8 +1,7 @@
 export const CHECKBOT_SEO = {
-  title:
-    "Language AI Checkbot: Check Grammar, Spelling and Punctuation errors",
-  description: `Check Grammar, Spelling and Punctuation errors for free with Language AI Checkbot. Improve your text
-  and check Grammar, Spelling and Punctuation errors`,
-  keywords: `grammar check, free grammar check, spelling check, spelling errors, punctuation errors, improve text, ai grammar check`,
+  title: "Free Grammar Checker - Language AI Checkbot",
+  description: `Grammar check for free! Copy and Paste your text in the online editor and hit the button to fix all grammar, spelling, and punctuation errors using cutting-edge AI technology.
+  Find the best words to improve any text instantly using Language AI Checkbot grammar checker.`,
+  keywords: `free grammar check, grammar check, check grammar, grammar checker, AI grammar check, spelling check, spell check, proofread, English, Proofreading, Text to Speech, Learning English, English Writing, Dyslexia, paraphrase, paraphrasing tool, rephrase, synonyms, smart synonyms, rewrite`,
   url: `${process.env.NEXT_PUBLIC_BASE_URL}/checkbot`,
 };
--- src/modules/home/lib/constant.tsx
+++ src/modules/home/lib/constant.tsx
@@ -1,9 +1,10 @@
 export const HOME_SEO = {
   title:
-    "Language AI: Terjemahan bahasa dan cek Grammar ke lebih dari 100 bahasa",
-  description: `Terjemahan teks & berkas secara akurat ke lebih dari 100 bahasa lainnya. 
-  Cek grammar, spelling, dan punctuation secara gratis dari 100 bahasa`,
-  keywords: `terjemahan, penerjemah, grammar check, free grammar check, terjemahan Indonesia, terjemahan Inggris, 
-  spelling check, spelling errors, punctuation errors, improve text, ai grammar check`,
+    "Free Grammar Checker, Language AI: Penerjemah paling akurat di seluruh dunia",
+  description: `Grammar check for free! Copy and Paste your text in the online editor and hit the button to fix all grammar, spelling, and punctuation errors using cutting-edge AI technology.
+  Layanan AI Translate yang ditawarkan tanpa biaya ini dapat langsung menerjemahkan berbagai kata dan frasa ke bahasa Indonesia dan lebih dari 100 bahasa lainnya.`,
+  keywords: `free grammar check, grammar check, check grammar, grammar checker, AI grammar check, spelling check, spell check, 
+  translate, terjemahkan, terjemahan, penerjemahan, penerjemah, terjemahan mesin, terjemahan online,
+  proofread, English, Proofreading, Text to Speech, Learning English, English Writing, Dyslexia, paraphrase, paraphrasing tool, rephrase, synonyms, smart synonyms, rewrite`,
   url: process.env.NEXT_PUBLIC_BASE_URL,
 };
