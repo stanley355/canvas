47d5bdf58d6d73ae8e5a4f9504bf6da1dec98808
Stanley Winata <winatastanley355@gmail.com>
2024-01-10 07:11:56 +0700

    Fix

--- src/modules/checkbot/lib/constant.ts
+++ src/modules/checkbot/lib/constant.ts
@@ -3,5 +3,5 @@ export const CHECKBOT_SEO = {
   description: `Grammar check for free! Copy and Paste your text in the online editor and hit the button to fix all grammar, spelling, and punctuation errors using cutting-edge AI technology.
   Find the best words to improve any text instantly using Language AI Checkbot grammar checker.`,
   keywords: `free grammar check, grammar check, check grammar, grammar checker, AI grammar check, spelling check, spell check, proofread, English, Proofreading, Text to Speech, Learning English, English Writing, Dyslexia, paraphrase, paraphrasing tool, rephrase, synonyms, smart synonyms, rewrite`,
-  url: `${process.env.NEXT_PUBLIC_BASE_URL}/checkbot`,
+  url: `${process.env.NEXT_PUBLIC_BASE_URL}checkbot`,
 };
