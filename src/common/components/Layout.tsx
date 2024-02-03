eb10373526986ed927b8805f87a2941a3d03b106
Stanley Winata <winatastanley355@gmail.com>
2024-02-03 14:07:56 +0700

    Layout

--- src/common/components/Layout.tsx
+++ src/common/components/Layout.tsx
@@ -1,5 +1,4 @@
 import { Inter } from "next/font/google";
-import { useRouter } from "next/router";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Header from "./Header";
@@ -8,13 +7,11 @@ import Footer from "./Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const router = useRouter();
-
   return (
     <div className={inter.className}>
-      {!router.pathname.includes("document") && <Header />}
+      <Header />
       <main className="h-min-screen pt-16 lg:pt-12">{children}</main>
-      {!router.pathname.includes("document") && <Footer />}
+      <Footer />
       <ToastContainer
         position="top-center"
         autoClose={2000}
