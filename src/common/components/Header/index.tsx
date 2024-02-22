a394bdbf516b6da2a4a513bfcc43fdfebe9f36ab
Stanley Winata <winatastanley355@gmail.com>
2024-02-22 15:26:07 +0700

    Header mobile

--- src/common/components/Header/index.tsx
+++ src/common/components/Header/index.tsx
@@ -1,41 +1,16 @@
-import { useState, useMemo } from "react";
-import Image from "next/image";
-import Link from "next/link";
+import { useMemo } from "react";
 import cookie from "js-cookie";
-import { RxHamburgerMenu } from "react-icons/rx";
-import HeaderMobileMenu from "./HeaderMobileMenu";
+import HeaderMobile from "./HeaderMobile";
 
 const Header = () => {
-  const [showMenu, setShowMenu] = useState(false);
   const isLogin = useMemo(() => {
     const cookieToken = cookie.get("token");
     return Boolean(cookieToken);
   }, []);
 
   return (
-    <nav className="w-full fixed z-10 left-0 top-0 border-b p-2 bg-white flex justify-between items-center">
-      <Link href="/" className="flex items-center">
-        <Image
-          src="/images/languageai.png"
-          alt="LanguageAI"
-          width={35}
-          height={35}
-        />
-        <span>languageai</span>
-      </Link>
-      <button
-        type="button"
-        className="p-2 h-full"
-        onClick={() => setShowMenu(true)}
-      >
-        <RxHamburgerMenu />
-      </button>
-      {showMenu && (
-        <HeaderMobileMenu
-          isLogin={isLogin}
-          onCloseClick={() => setShowMenu(false)}
-        />
-      )}
+    <nav>
+      <HeaderMobile isLogin={isLogin} />
     </nav>
   );
 };
