8118a5870b505c31d27c13a5e48f02fabca301b6
Stanley Winata <winatastanley355@gmail.com>
2024-02-29 13:37:48 +0700

    Woi

--- src/common/components/Layout.tsx
+++ src/common/components/Layout.tsx
@@ -6,7 +6,7 @@ import Footer from "./Footer";
 const Layout = ({ children }: { children: React.ReactNode }) => {
 
   return (
-    <div className="">
+    <>
       <Header />
       <main className="min-h-screen pt-16 lg:pt-12">{children}</main>
       <Footer />
@@ -18,7 +18,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         pauseOnHover
         theme="light"
       />
-    </div>
+    </>
   );
 };
 
