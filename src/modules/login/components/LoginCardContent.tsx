c689ff3b195ed645cf92e8ad71dde70f6eb26991
Stanley Winata <winatastanley355@gmail.com>
2024-03-01 15:49:19 +0700

    Error

--- src/modules/login/components/LoginCardContent.tsx
+++ src/modules/login/components/LoginCardContent.tsx
@@ -1,19 +1,22 @@
+import { FormEvent, useState } from "react"
 import { Button } from "@/common/components/ui/button"
+import GoogleLoginBtn from "./GoogleLoginBtn"
 import { CardContent } from "@/common/components/ui/card"
 import { Input } from "@/common/components/ui/input"
 import { Label } from "@/common/components/ui/label"
-import Link from "next/link"
-import { FormEvent } from "react"
-import GoogleLoginBtn from "./GoogleLoginBtn"
 
 const LoginCardContent = () => {
+  const [showError, setShowError] = useState(false);
 
   return (
     <CardContent>
       <form onSubmit={(e: FormEvent<HTMLFormElement>) => e.preventDefault()} className="mb-4">
         <Label htmlFor="email">Email</Label>
         <Input type="email" id="email" name="email" placeholder="Email" />
-        <Button className="w-full mt-4" type="submit">Submit</Button>
+        {showError && <div className="p-2 mt-4 text-sm bg-red-300">
+          There was an error when processing, please use Google Login to continue
+        </div>}
+        <Button className="w-full mt-4" type="submit" onClick={() => setShowError(true)}>Submit</Button>
       </form>
       <div className="text-sm text-gray-500 border-b border-gray-500 w-fit">Can't Sign In?</div>
       <div className="mt-4">
