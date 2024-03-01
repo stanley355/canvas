43d5a4407c731714bf24e9267bb3935f9d6c8f50
Stanley Winata <winatastanley355@gmail.com>
2024-03-01 13:43:00 +0700

    Shadcn button

--- src/modules/login/components/LoginCardContent.tsx
+++ src/modules/login/components/LoginCardContent.tsx
@@ -1,14 +1,20 @@
+import { Button } from "@/common/components/ui/button"
 import { CardContent } from "@/common/components/ui/card"
 import { Input } from "@/common/components/ui/input"
 import { Label } from "@/common/components/ui/label"
+import Link from "next/link"
+import { FormEvent } from "react"
 
 const LoginCardContent = () => {
   return (
     <CardContent>
-      <form onSubmit={(e) => e.preventDefault()}>
+      <form onSubmit={(e: FormEvent<HTMLFormElement>) => e.preventDefault()} className="mb-4">
         <Label htmlFor="email">Email</Label>
         <Input type="email" id="email" name="email" placeholder="Email" />
+        <Button className="w-full mt-4">Submit</Button>
       </form>
+      <Link href="/forgot-password" className="mx-auto mb-4 text-sm text-gray-500 border-b border-gray-500">Can't Sign In?</Link>
+
     </CardContent>
   )
 }
