c4daf4fed2d7596fce9958d37f6ebb8977d84867
Stanley Winata <winatastanley355@gmail.com>
2024-03-01 14:09:41 +0700

    Header

--- src/modules/login/components/LoginCardHeader.tsx
+++ src/modules/login/components/LoginCardHeader.tsx
@@ -1,13 +1,15 @@
 import Link from 'next/link'
 import { CardHeader, CardTitle } from '@/common/components/ui/card'
+import { useState } from 'react'
 
 const LoginCardHeader = () => {
+  const [isRegister, setIsRegister] = useState(false);
   return (
     <CardHeader className="flex flex-row items-center justify-between pt-4">
-      <CardTitle>Login</CardTitle>
-      <Link href={"/"} className="text-sm border-b border-black cursor-pointer">
-        I don't have an account
-      </Link>
+      <CardTitle>{isRegister ? "Register" : "Login"}</CardTitle>
+      <button className="text-sm border-b border-black cursor-pointer" onClick={() => setIsRegister(!isRegister)}>
+        {isRegister ? "I have an account" : "I don't have an account"}
+      </button>
     </CardHeader>
   )
 }
