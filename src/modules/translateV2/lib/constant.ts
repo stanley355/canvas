4d25fb7981b97f943e51ab825c1478935936358e
Stanley Winata <winatastanley355@gmail.com>
2024-03-04 19:24:38 +0700

    Translate

---
+++ src/modules/translateV2/lib/constant.ts
@@ -0,0 +1,153 @@
+export const TRANSLATE_LANGUAGE_LIST_COMMON = [
+  {
+    label: "English",
+    value: "English"
+  },
+  {
+    label: "Indonesian",
+    value: "Indonesian"
+  },
+  {
+    label: "Chinese (Simplified)",
+    value: "Chinese (Simplified)"
+  },
+]
+
+export const TRANSLATE_LANGUAGE_LIST_V2 = [
+  {
+    label: "African",
+    value: "African",
+  },
+  {
+    label: "Arabic",
+    value: "Arabic",
+  },
+  {
+    label: "Bulgarian",
+    value: "Bulgarian",
+  },
+  {
+    label: "Chinese (Simplified)",
+    value: "Chinese (Simplified)",
+  },
+  {
+    label: "Chinese (Traditional)",
+    value: "Chinese (Traditional)",
+  },
+  {
+    label: "Dutch",
+    value: "Dutch",
+  },
+  {
+    label: "English",
+    value: "English",
+  },
+  {
+    label: "Finnish",
+    value: "Finnish",
+  },
+  {
+    label: "French",
+    value: "French",
+  },
+  {
+    label: "German",
+    value: "German",
+  },
+  {
+    label: "Greek",
+    value: "Greek",
+  },
+  {
+    label: "Hebrew",
+    value: "Hebrew",
+  },
+  {
+    label: "Hindi",
+    value: "Hindi",
+  },
+  {
+    label: "Indonesian",
+    value: "Indonesian",
+  },
+  {
+    label: "Italian",
+    value: "Italian",
+  },
+  {
+    label: "Japanese",
+    value: "Japanese",
+  },
+  {
+    label: "Korean",
+    value: "Korean",
+  },
+  {
+    label: "Malay",
+    value: "Malay",
+  },
+  {
+    label: "Norwegian",
+    value: "Norwegian",
+  },
+  {
+    label: "Persian",
+    value: "Persian",
+  },
+  {
+    label: "Polish",
+    value: "Polish",
+  },
+  {
+    label: "Portuguese",
+    value: "Portuguese",
+  },
+  {
+    label: "Romanian",
+    value: "Romanian",
+  },
+  {
+    label: "Russian",
+    value: "Russian",
+  },
+  {
+    label: "Serbian",
+    value: "Serbian",
+  },
+  {
+    label: "Spanish",
+    value: "Spanish",
+  },
+  {
+    label: "Swedish",
+    value: "Swedish",
+  },
+  {
+    label: "Tagalog",
+    value: "Tagalog",
+  },
+  {
+    label: "Tamil",
+    value: "Tamil",
+  },
+  {
+    label: "Thai",
+    value: "Thai",
+  },
+  {
+    label: "Turkish",
+    value: "Turkish",
+  },
+  {
+    label: "Ukrainian",
+    value: "Ukrainian",
+  },
+  {
+    label: "Vietnamese",
+    value: "Vietnamese",
+  },
+  {
+    label: "Welsh",
+    value: "Welsh",
+  },
+];
