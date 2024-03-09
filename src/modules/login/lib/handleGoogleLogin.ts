32a589a615539c9d49c7498db48da6beef00d70c
Stanley Winata <winatastanley355@gmail.com>
2024-03-09 11:57:57 +0700

    Fix login flow

--- src/modules/login/lib/handleGoogleLogin.ts
+++ src/modules/login/lib/handleGoogleLogin.ts
@@ -12,7 +12,7 @@ export const handleGoogleLogin = async (token: any) => {
   if (loginRes.token) {
     sendFirebaseEvent("google_login");
     Cookies.set("token", loginRes.token);
-    window.location.href = "/account/";
+    window.location.reload();
     return loginRes;
   }
 
