a23fb240be2fa693d9a46dedf84d1e6ff397933b
Stanley Winata <winatastanley355@gmail.com>
2024-03-09 16:22:16 +0700

    Test

--- src/common/lib/api/fetchDatoCms.ts
+++ src/common/lib/api/fetchDatoCms.ts
@@ -10,6 +10,10 @@ export const fetchDatoCms = async (
     Authorization: `Bearer ${process.env.DATOCMS_API_TOKEN}`,
   };
 
+  console.log(url);
+  console.log(header);
+  
+  
   try {
     const datoCmsRes = await request(
       url,
@@ -18,6 +22,8 @@ export const fetchDatoCms = async (
       header
     );
 
+    console.log(datoCmsRes);
+    
     return datoCmsRes;
   } catch (error) {
     const errorRes = axiosErrorHandler(url, error);
