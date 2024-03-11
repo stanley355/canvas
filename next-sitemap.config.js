e57623dbae567aa3b7d4f8d7cc8da2f17c0ab087
Stanley Winata <winatastanley355@gmail.com>
2024-03-11 17:20:39 +0700

    Sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_BASE_URL,
   generateRobotsTxt: true,
-  exclude: ["/plans", "/account", "/document"],
+  exclude: ["/plans", "/account", "/account/subscription", "/document"],
   generateRobotsTxt: true,
   // Add any additional configuration options here
 };
