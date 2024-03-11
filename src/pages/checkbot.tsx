784a910099343a12ae1379d42ab8bbc295bd4f3f
Stanley Winata <winatastanley355@gmail.com>
2024-03-11 09:42:10 +0700

    Fix build

--- src/pages/checkbot.tsx
+++ src/pages/checkbot.tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next";
+import { GetServerSideProps} from "next";
 
 // Permament redirect, GrammarCheck was Checkbot before
 const CheckBot = () => {
@@ -6,7 +6,7 @@ const CheckBot = () => {
 };
 
 export default CheckBot;
-export const getStaticProps: GetStaticProps = () => {
+export const getServerSideProps: GetServerSideProps= async() => {
   return {
     redirect: {
       permanent: true,
