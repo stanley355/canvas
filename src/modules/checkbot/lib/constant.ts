86e3b89d8f57791a18d7a745c12d39274547b7df
Stanley Winata <winatastanley355@gmail.com>
2024-03-11 09:39:34 +0700

    Remove unused code

--- src/modules/checkbot/lib/constant.ts
+++
@@ -1,7 +0,0 @@
-export const CHECKBOT_SEO = {
-  title: "Free Grammar Checker - Language AI Checkbot",
-  description: `Grammar check for free! Copy and Paste your text in the online editor and hit the button to fix all grammar, spelling, and punctuation errors using cutting-edge AI technology.
-  Find the best words to improve any text instantly using Language AI Checkbot grammar checker.`,
-  keywords: `free grammar check, grammar check, check grammar, grammar checker, AI grammar check, spelling check, spell check, proofread, English, Proofreading, Text to Speech, Learning English, English Writing, Dyslexia, paraphrase, paraphrasing tool, rephrase, synonyms, smart synonyms, rewrite`,
-  url: `${process.env.NEXT_PUBLIC_BASE_URL}checkbot`,
-};
