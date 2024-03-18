6ba6573911f4f8469de6d2359acb698ae180f33b
Stanley Winata <winatastanley355@gmail.com>
2024-03-18 12:41:43 +0700

    Fix build

--- src/pages/account/subscription.tsx
+++ src/pages/account/subscription.tsx
@@ -1,13 +1,27 @@
+import { GetServerSideProps } from "next";
 import AccountPlanDetail from "@/modules/account/components/AccountPlanDetail";
-import AccountPlanList from "@/modules/account/components/AccountPlanList";
+import { getAccountPageServerProps } from "@/modules/account/lib/getAccountPageServerProps";
+import { IUser } from "@/common/lib/api/users/interfaces";
+import { ISubscription } from "@/common/lib/api/subscriptions/interfaces";
+import { ITopup } from "@/common/lib/api/topups/interfaces";
 
-const AccountSubscription = () => {
+interface IAccountSubscription {
+  account: {
+    user: IUser,
+    active_subscription: ISubscription,
+    topups: ITopup[]
+  };
+}
+
+const AccountSubscription = (props: IAccountSubscription) => {
+  const { account } = props;
   return (
     <div className="container mx-auto mt-16 lg:mt-4 lg:px-12">
-      <AccountPlanDetail />
-      <AccountPlanList />
+      <AccountPlanDetail account={account} />
     </div>
   );
 };
 
 export default AccountSubscription;
+
+export const getServerSideProps: GetServerSideProps = getAccountPageServerProps;
