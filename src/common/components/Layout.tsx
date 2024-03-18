1b5c5a01db8db2634a21549a023e2fb9db2e94d7
Stanley Winata <winatastanley355@gmail.com>
2024-03-18 15:59:09 +0700

    Premium

--- src/common/components/Layout.tsx
+++ src/common/components/Layout.tsx
@@ -15,7 +15,8 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       "/account/subscription",
       "/translate",
       "/grammar-check",
-      "/plans/payasyougo"
+      "/plans/payasyougo",
+      "/plans/premium"
     ].includes(router.pathname);
   }, [router.pathname]);
 
