87ef4ec7742be52901299937c025c8f604404838
Stanley Winata <winatastanley355@gmail.com>
2024-03-18 12:39:26 +0700

    Cleanup

--- src/common/lib/api/documents/documentInterface.ts
+++
@@ -1,9 +0,0 @@
-export interface IDocument {
-  id: string;
-  user_id: string;
-  created_at: string;
-  updated_at: string;
-  name: string;
-  content: string | null;
-  checkbot_completion: string | null;
-}
