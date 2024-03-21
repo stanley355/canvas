7c0f30fe4e6451be636fd8ac0a1cfef78be03b08
Stanley Winata <winatastanley355@gmail.com>
2024-03-21 15:26:10 +0700

    Nofollow and index for world

--- src/common/components/MetaHead.tsx
+++ src/common/components/MetaHead.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import React from "react";
 
 interface IMetaHeadSeo {
@@ -21,13 +22,15 @@ export interface IMetaHead {
 
 const MetaHead = (props: IMetaHead) => {
   const { pagesSchema } = props;
+  console.log(window.location.origin);
+  
   return (
     <Head>
       {/* meta */}
       <title>{pagesSchema.seo.title}</title>
       <meta name="description" content={pagesSchema.seo.description} />
       <meta name="keywords" content={pagesSchema.keywords} />
-      <meta name="robots" content="follow, index" />
+      <meta name="robots" content={window.location.origin === "https://languageai.world" ? "nofollow, noindex" : "follow, index"} />
       <meta charSet="UTF-8" />
       <meta
         name="viewport"
