3408d3d25b1c2beb6cb4aec85009cff4763361f2
Stanley Winata <winatastanley355@gmail.com>
2024-03-22 13:41:17 +0700

    Image to text seo

--- src/pages/image-to-text.tsx
+++ src/pages/image-to-text.tsx
@@ -1,16 +1,25 @@
 import { useState  } from 'react';
+import { GetStaticProps } from 'next';
 import { TbPhotoAi} from 'react-icons/tb';
 import Link from 'next/link';
 
 import ImageToTextInput from '@/modules/image-to-text/components/ImageToTextInput';
 import ImageToTextResult from '@/modules/image-to-text/components/ImageToTextResult';
+import { fetchDatoCms } from '@/common/lib/api/fetchDatoCms';
+import { getPagesSchema } from '@/common/lib/api/gql';
+import MetaHead, { IMetaHead } from '@/common/components/MetaHead';
 
-const ImageToText = () => {
+interface IImageToText {
+  datoCmsData: IMetaHead;
+}
+
+const ImageToText = (props: IImageToText) => {
+  const {datoCmsData} = props;
   const [recognizedText, setRecognizedText] = useState('');
-
 
   return (
     <div className='container px-0 pb-8 mx-auto mt-16 lg:mt-4 lg:px-4'>
+      <MetaHead pagesSchema={datoCmsData.pagesSchema} />
       <div className="flex items-center gap-1 px-3 py-1 ml-4 text-blue-800 bg-blue-100 border border-gray-300 rounded-md w-fit">
         <TbPhotoAi className="text-xl" />
         <span>Image to Text</span>
@@ -34,3 +43,14 @@ const ImageToText = () => {
 }
 
 export default ImageToText;
+export const getStaticProps: GetStaticProps = async () => {
+  const datoCmsData = await fetchDatoCms(getPagesSchema, {
+    slug: "image-to-text",
+  });
+
+  return {
+    props: {
+      datoCmsData,
+    },
+  };
+};
