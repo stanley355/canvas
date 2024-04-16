a948e319a6f97aaf14dc33036b74fdc3f650d5d3
Stanley Winata <winatastanley355@gmail.com>
2024-04-16 15:25:37 +0700

    Account student form

--- src/pages/account/students.tsx
+++ src/pages/account/students.tsx
@@ -1,8 +1,16 @@
+import AccountStudentForm from "@/modules/account/components/AccountStudentForm";
 import { getAccountStudentPageServerProps } from "@/modules/account/lib/getAccountStudentPageServerSideProps";
 import { GetServerSideProps } from "next";
 
-const AccountStudents = () => {
-  return <div></div>
+interface IAccountStudents {
+  studentAvailability: Record<string, boolean>
+}
+
+const AccountStudents = (props: IAccountStudents) => {
+  const {studentAvailability} = props;
+  // is_student && !if_free_discount && !is_half_discount && can_reapply 
+  // !is_student && can_reapply
+  return <AccountStudentForm />
 }
 
 export default AccountStudents;
