ad967ac7a7a5b241e1b4add4309626ccdd010b1c
Stanley Winata <winatastanley355@gmail.com>
2024-04-17 15:09:21 +0700

    Account student plan detail

--- src/modules/account/components/AccountPlanDetail.tsx
+++ src/modules/account/components/AccountPlanDetail.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { ISubscription } from "@/common/lib/api/subscriptions/interfaces";
 import { ITopup } from "@/common/lib/api/topups/interfaces";
 import { IUser } from "@/common/lib/api/users/interfaces";
@@ -6,10 +5,13 @@ import AccountFreePlanDetail from "./AccountFreePlanDetail";
 import AccountPlanList from "./AccountPlanList";
 import AccountPayasyougoPlanDetail from "./AccountPayasyougoPlanDetail";
 import AccountPremiumPlanDetail from "./AccountPremiumPlanDetail";
+import { IStudent } from "@/common/lib/api/students/interfaces";
+import AccountStudentPlanDetail from "./AccountStudentPlanDetail";
 
 interface IAccountPlanDetail {
   account: {
     user: IUser;
+    active_student_discount: IStudent;
     active_subscription: ISubscription;
     topups: ITopup[];
   };
@@ -17,7 +19,11 @@ interface IAccountPlanDetail {
 
 const AccountPlanDetail = (props: IAccountPlanDetail) => {
   const { account } = props;
-  const { user, active_subscription, topups } = account;
+  const { user, active_subscription, topups, active_student_discount } = account;
+
+  if (active_student_discount.student_application_valid) {
+    return <AccountStudentPlanDetail student={active_student_discount} topups={topups} />
+  }
 
   if (active_subscription && active_subscription?.id) {
     return (
---
+++ src/modules/account/components/AccountStudentPlanDetail.tsx
@@ -0,0 +1,41 @@
+import AccountTopupTable from "./AccountTopupTable";
+import { ITopup } from "@/common/lib/api/topups/interfaces";
+import { IStudent } from "@/common/lib/api/students/interfaces";
+
+interface IAccountStudentPlanDetail {
+  student: IStudent;
+  topups: ITopup[];
+}
+
+const AccountStudentPlanDetail = (props: IAccountStudentPlanDetail) => {
+  const { student, topups } = props;
+  
+  const isFreeDiscount = new Date(student.free_discount_end_at).getTime() > new Date().getTime();
+
+  return (
+    <div className="pb-4">
+      <div className="mb-4 text-2xl font-bold border-b">Subscription</div>
+      
+      <div className="mb-4 text-xl font-bold">Plan Details</div>
+      <div className="mb-4">
+        <div className="mb-2 text-gray-500">Your Plan</div>
+        <div className="mb-2">Student Plan {isFreeDiscount ?'(One Year Free)' : '(50% Discount)'}</div>
+
+        <div className="mb-2 text-gray-500">Student Plan Start date</div>
+        <div className="mb-2">
+          {new Date(student.created_at).toLocaleDateString("id-ID")}
+        </div>
+        <div className="mb-2 text-gray-500">{isFreeDiscount ?  'One Year Free' : 'Half Discount'} end date</div>
+        <div>{new Date(isFreeDiscount ? student.free_discount_end_at : student.half_discount_end_at).toLocaleDateString("id-ID")}</div>
+      </div>
+
+      <div className="w-full p-2 mb-4 text-sm bg-blue-100 lg:w-1/2">
+        Your usage is unlimited until the end of your plan
+      </div>
+
+      {topups && topups?.length > 0 && <AccountTopupTable topups={topups} />}
+    </div>
+  );
+};
+
+export default AccountStudentPlanDetail;
