83769b18bae2f00b1405ad83ab7c16a267492dd6
Stanley Winata <winatastanley355@gmail.com>
2024-04-18 10:18:49 +0700

    Sitemap

--- src/pages/account/subscription.tsx
+++ src/pages/account/subscription.tsx
@@ -4,10 +4,12 @@ import { getAccountPageServerProps } from "@/modules/account/lib/getAccountPageS
 import { IUser } from "@/common/lib/api/users/interfaces";
 import { ISubscription } from "@/common/lib/api/subscriptions/interfaces";
 import { ITopup } from "@/common/lib/api/topups/interfaces";
+import { IStudent } from "@/common/lib/api/students/interfaces";
 
 interface IAccountSubscription {
   account: {
     user: IUser;
+    active_student_discount: IStudent;
     active_subscription: ISubscription;
     topups: ITopup[];
   };
@@ -17,7 +19,7 @@ const AccountSubscription = (props: IAccountSubscription) => {
   const { account } = props;
   return (
     <div className="container mx-auto mt-16 lg:mt-4 lg:px-12">
-      <AccountPlanDetail account={account} />
+      <AccountPlanDetail account={account}  />
     </div>
   );
 };
