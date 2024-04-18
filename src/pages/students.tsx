bf5b2d55edbb81dff841dd4c2ee819e1c6240627
Stanley Winata <winatastanley355@gmail.com>
2024-04-18 13:14:45 +0700

    Students hero

---
+++ src/pages/students.tsx
@@ -0,0 +1,13 @@
+import StudentsHero from "@/modules/students/components/StudentsHero"
+
+const Students = () => {
+  return (
+    <div>
+      <div className="bg-indigo-900">
+        <StudentsHero />
+      </div>
+    </div>
+  )
+}
+
+export default Students
