4fbf03a5fc42510f6ea594fd8f2c349db2d1662c
Stanley Winata <winatastanley355@gmail.com>
2024-04-23 10:22:27 +0700

    Fix clarity

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,17 +3,19 @@ import FirebaseProvider from "@/common/components/FirebaseProvider";
 import Layout from "@/common/components/Layout";
 import "@/styles/globals.scss";
 import "@/styles/shadcn.scss";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <FirebaseProvider>
-      <script type="text/javascript">
+
+      <Script type="text/javascript" strategy="afterInteractive">
         {`(function(c,l,a,r,i,t,y){
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
           })(window, document, "clarity", "script", "m0voqp1w10");`}
-      </script>
+      </Script>
       <Layout>
         <Component {...pageProps} />
       </Layout>
