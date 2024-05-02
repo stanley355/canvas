a132e4acf283abe35a03ea2b2e39e75d5fed09ca
Stanley Winata <winatastanley355@gmail.com>
2024-05-02 09:05:52 +0700

    Prompts v2

---
+++ src/pages/api/authorv2/prompts.ts
@@ -0,0 +1,31 @@
+import { axiosErrorHandler } from "@/common/lib/api/axiosErrorHandler";
+import axios from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const authorV2PromptsAPI = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  let URL = `${process.env.AUTHOR_URL}v2/prompts`;
+
+  if (req.headers && req.headers.path) {
+    URL += req.headers.path;
+  }
+  const axiosConfig = {
+    method: req.method,
+    url: URL,
+    data: req.body,
+    headers: {
+      Authorization: process.env.AUTHOR_TOKEN,
+    },
+  };
+
+  try {
+    const { data } = await axios(axiosConfig);
+    res.json(data);
+  } catch (err: any) {
+    res.status(err.response.status).send(err);
+  }
+};
+
+export default authorV2PromptsAPI;
