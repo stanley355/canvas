7764e218b8840653975dd7dfc27b207f16a909b9
Stanley Winata <winatastanley355@gmail.com>
2024-05-03 15:32:49 +0700

    Login gmail v2

--- src/modules/login/lib/handleGoogleLogin.ts
+++ src/modules/login/lib/handleGoogleLogin.ts
@@ -2,20 +2,20 @@ import { toast } from "react-toastify";
 import Cookies from "js-cookie";
 import { JwtPayload, decode } from "jsonwebtoken";
 import { sendFirebaseEvent } from "@/common/lib/firebase/sendFirebaseEvent";
-import { fetchUserGmailLogin } from "@/common/lib/api/users/fetchUserGmailLogin";
+import { fetchUsersV2LoginGmail } from "@/common/lib/apiV2/users/fetchUsersV2LoginGmail";
 
 export const handleGoogleLogin = async (token: any) => {
   sendFirebaseEvent("login");
   const decodedToken = decode(String(token.credential)) as JwtPayload;
-  const loginRes = await fetchUserGmailLogin(decodedToken);
+  const loginRes = await fetchUsersV2LoginGmail(decodedToken);
 
-  if (loginRes.token) {
+  if (loginRes.token !== "") {
     sendFirebaseEvent("google_login");
     Cookies.set("token", loginRes.token);
     window.location.href = "/account/";
     return loginRes;
   }
 
-  toast.error(loginRes.data.message);
+  toast.error("Server busy, please try again");
   return loginRes;
 };
---
+++ src/pages/api/authorv2/users.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const authorV2UsersAPI = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  let URL = `${process.env.AUTHOR_URL}v2/users`;
+
+  if (req.headers && req.headers.path) {
+    URL += req.headers.path;
+  }
+  const axiosConfig = {
+    method: req.method,
+    url: URL,
+    data: req.body,
+    headers: {
+      Authorization: process.env.AUTHOR_TOKEN,
+    },
+  };
+
+  try {
+    const { data } = await axios(axiosConfig);
+    res.json(data);
+  } catch (err: any) {
+    res.status(err.response.status).json(err.response.data);
+  }
+};
+
+export default authorV2UsersAPI;
