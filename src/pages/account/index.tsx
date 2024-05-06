9ce7467ae279b8737f66ead6112c1c4db3e7034b
Stanley Winata <winatastanley355@gmail.com>
2024-05-06 11:23:15 +0700

    Handle if account page backend return error

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -8,6 +8,8 @@ import { IUser } from "@/common/lib/api/users/interfaces";
 import { ISubscription } from "@/common/lib/api/subscriptions/interfaces";
 import { ITopup } from "@/common/lib/api/topups/interfaces";
 import { IStudent } from "@/common/lib/api/students/interfaces";
+import CanvasButton from "@/common/components/ui/CanvasButton";
+import { TbReload } from "react-icons/tb";
 
 interface IAccountProps {
   account: {
@@ -21,11 +23,21 @@ interface IAccountProps {
 const Account = (props: IAccountProps) => {
   const { account } = props;
 
+  if (!account.user) {
+    return <div className="container px-6 mx-auto mt-16 lg:mt-4 lg:px-12">
+      <div className="mt-40 text-xl text-center text-gray-500">Application error, please refresh and try again</div>
+      <CanvasButton className="gap-2 px-4 mx-auto mt-8 text-lg" onClick={() => window.location.reload()}>
+        <TbReload />
+        Refresh
+      </CanvasButton>
+    </div>
+  }
+  
   return (
     <div className="container px-6 mx-auto mt-16 lg:mt-4 lg:px-12">
       <div className="lg:grid lg:grid-cols-2">
-        <AccountDetail user={account.user} />
-        <AccountPlanDetail account={account} />
+        {/* <AccountDetail user={account.user} />
+        <AccountPlanDetail account={account} /> */}
       </div>
     </div>
   );
