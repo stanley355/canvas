c67f498fc41ce0e2f2c8cd32b037c54a1ae09700
Stanley Winata <winatastanley355@gmail.com>
2024-06-23 15:19:22 +0700

    Update login card and remove unused component

--- src/modules/login/components/LoginCard.tsx
+++ src/modules/login/components/LoginCard.tsx
@@ -1,15 +1,18 @@
-import { Card } from "@/common/components/ui/card";
-import LoginCardHeader from "./LoginCardHeader";
-import LoginCardContent from "./LoginCardContent";
-import LoginCardFooter from "./LoginCardFooter";
+import GoogleLoginBtn from "./GoogleLoginBtn";
 
 const LoginCard = () => {
   return (
-    <Card className="border-transparent w-full lg:w-[350px] lg:mx-auto lg:border-black mt-4">
-      <LoginCardHeader />
-      <LoginCardContent />
-      <LoginCardFooter />
-    </Card>
+    <div className="p-4 py-6 bg-blue-100 border border-blue-400 rounded-lg lg:w-[400px] mt-8 mx-auto">
+      <div className="p-1 mb-4 text-sm bg-blue-200 rounded-md">We are experiencing issues with Password login, but you can login with your Gmail Account</div>
+      <h1 className="mb-6 text-2xl font-semibold text-center text-brand-primary">Login in one click</h1>
+      <div className="flex items-center justify-center mb-4 animate-bounce">
+        <GoogleLoginBtn />
+      </div>
+
+      <div>
+        By signing up, you agree to the <b>Terms and Conditions</b> and <b> Privacy Policy</b>.
+      </div>
+    </div>
   );
 };
 
