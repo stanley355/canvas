278d891d347e60a5853b97e6bd56f602caf89827
Stanley Winata <winatastanley355@gmail.com>
2024-06-26 07:48:43 +0700

    Add firebase event to handleGoogleLogin

---
+++ src/common/lib/firebase/firebaseEventNames.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_EVENT_NAMES = {
+  login: {
+    login: "login",
+    loginGoogle: "login_google"
+  },
+  click: {
+    login: "click_login"
+  }
+}
--- src/modules/login/lib/handleGoogleLogin.ts
+++ src/modules/login/lib/handleGoogleLogin.ts
@@ -4,15 +4,16 @@ import { JwtPayload, decode } from "jsonwebtoken";
 import { sendFirebaseEvent } from "@/common/lib/firebase/sendFirebaseEvent";
 import { fetchUsersLoginGmail } from "@/common/lib/api/users/fetchUsersLoginGmail";
 import { LOGIN_FAIL_MESSAGE } from "./constant";
+import { FIREBASE_EVENT_NAMES } from "@/common/lib/firebase/firebaseEventNames";
 
 export const handleGoogleLogin = async (token: any) => {
-  sendFirebaseEvent("login");
-  sendFirebaseEvent("login_google");
-
+  sendFirebaseEvent(FIREBASE_EVENT_NAMES.click.login);
   const decodedToken = decode(String(token.credential)) as JwtPayload;
   const loginRes = await fetchUsersLoginGmail(decodedToken);
 
   if (loginRes?.token) {
+    sendFirebaseEvent(FIREBASE_EVENT_NAMES.login.login);
+    sendFirebaseEvent(FIREBASE_EVENT_NAMES.login.loginGoogle);
     Cookies.set("token", loginRes.token);
     const redirectPath =
       window.location.pathname === "/" || window.location.pathname === "/login"
