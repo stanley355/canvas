451b480a3a4219a7b3b61ca80616a33f85bf78a5
Stanley Winata <winatastanley355@gmail.com>
2024-06-26 08:04:21 +0700

    Remove unused code

--- src/common/lib/api/users/fetchUsersAccount.ts
+++ src/common/lib/api/users/fetchUsersAccount.ts
@@ -1,10 +1,16 @@
 import axios from "axios";
-import Cookies from "js-cookie";
-import { JwtPayload, decode } from "jsonwebtoken";
+import { IUser } from "./interfaces";
+
+interface IResponse {
+  user: IUser;
+  active_student_discount: Record<string, string | number>;
+  active_subscription: Record<string, string | number>;
+  topups: Record<string, string | number>[];
+}
 
 export const fetchUsersAccount = async (
   id: string
-): Promise<any > => {
+): Promise<IResponse> => {
   
   const url = `${process.env.NEXT_PUBLIC_BASE_URL}api/users/account?id=${id}`;
 
