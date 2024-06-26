d4c5c3584d7342d3187381542f8c3c4049d8b250
Stanley Winata <winatastanley355@gmail.com>
2024-06-26 09:12:41 +0700

    Update colors

--- src/common/components/constant.ts
+++ src/common/components/constant.ts
@@ -8,9 +8,9 @@ export interface INextButtonVariants {
 
 export const NEXT_BUTTON_VARIANTS = {
   default:
-    "bg-gradient-to-b from-blue-600 to-blue-700 hover:from-blue-700 hover:to-blue-800 px-4 py-2 rounded-lg text-white flex items-center",
+    "bg-gradient-to-b from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 px-4 py-2 rounded-lg text-white flex items-center",
   outline:
-    "bg-white text-blue-500 border border-blue-500 hover:text-blue-700 hover:border-blue-700  px-4 py-2 rounded-lg flex items-center",
+    "bg-white text-blue-400 border border-blue-400 hover:text-blue-500 hover:border-blue-500  px-4 py-2 rounded-lg flex items-center",
   disabled: "bg-blue-100 text-blue-400 px-4 py-2 rounded-lg flex items-center",
   "disabled-outline":
     "bg-white border border-blue-100 text-blue-100  px-4 py-2 rounded-lg flex items-center",
