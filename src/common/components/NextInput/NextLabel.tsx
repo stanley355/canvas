3fe78ee5f6fa133f524f997f49145dfadf8c390c
Stanley Winata <winatastanley355@gmail.com>
2024-06-27 08:01:24 +0700

    NextLabel

---
+++ src/common/components/NextInput/NextLabel.tsx
@@ -0,0 +1,18 @@
+
+export interface NextLabelProps 
+  extends React.LabelHTMLAttributes<HTMLLabelElement> {
+ required: boolean; 
+}
+
+
+const NextLabel = (props: NextLabelProps) => {
+  const {required,children, ...rest} = props;
+  return (
+    <label {...rest}>
+     {props.children} 
+     {required && <span className="text-red-500">*</span> }
+    </label>
+  )
+}
+
+export default NextLabel 
--- src/common/components/NextInput/index.tsx
+++ src/common/components/NextInput/index.tsx
@@ -1,13 +1,13 @@
 import { cn } from "@/common/lib/cn";
 import { NEXT_INPUT_VARIANTS, INextInputVariants } from "./variants";
 
-export interface InputProps
+export interface NextInputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   variants?: keyof INextInputVariants
 }
 
 
-const NextInput = (props: InputProps) => {
+const NextInput = (props: NextInputProps) => {
   const { className, variants, ...rest } = props;
   return (
     <input
