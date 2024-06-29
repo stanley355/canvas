40fe8a8cb42de94fda575cac3d000ec33cc89503
Stanley Winata <winatastanley355@gmail.com>
2024-06-29 14:11:29 +0700

    Button

--- src/common/components/NextButton/variants.ts
+++ src/common/components/NextButton/variants.ts
@@ -8,6 +8,6 @@ export const NEXT_BUTTON_VARIANTS = {
   default:
     "bg-gradient-to-b from-blue-800 to-blue-900 hover:from-blue-900 hover:to-brand-primary px-4 py-2 rounded-lg text-white flex items-center gap-1",
   outline:
-    "bg-white text-blue-800 border border-blue-800 hover:text-brand-primary hover:border-brand-primary px-4 py-2 rounded-lg flex items-center gap-1",
+    "bg-white border border-blue-800 hover:border-brand-primary px-4 py-2 rounded-lg flex items-center gap-1",
   none: "",
 };
--- src/common/components/NextSelect.tsx
+++ src/common/components/NextSelect.tsx
@@ -32,7 +32,7 @@ const NextSelect = (props: NextSelectProps) => {
       <NextButton
         type="button"
         variant="outline"
-        className={cn("gap-1 w-full justify-between text-black", selectClassname)}
+        className={cn("gap-1 w-full justify-between", selectClassname)}
         onClick={() => setShowOptions(!showOptions)}
       >
         {selectedOption.label}
@@ -41,7 +41,7 @@ const NextSelect = (props: NextSelectProps) => {
 
       <div
         className={cn(
-          "absolute top-12 left-0 flex flex-col rounded-lg border border-blue-600 w-full",
+          "absolute top-12 left-0 flex flex-col rounded-lg border border-brand-primary w-full",
           showOptions ? "visible h-auto" : "invisible h-0",
           optionContainerClassname)}
       >
@@ -49,7 +49,7 @@ const NextSelect = (props: NextSelectProps) => {
           <NextButton
             type="button"
             variant="none"
-            className={cn("px-4 py-2 w-full text-left z-10", optionClassname)}
+            className={cn("px-4 py-2 w-full text-left z-10 hover:bg-blue-100 rounded-lg", optionClassname)}
             key={String(option.label)}
             onClick={() => {
               setSelectedOption(option);
