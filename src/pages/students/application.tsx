085fd07d41c127673a1897f79e19da54379adf46
Stanley Winata <winatastanley355@gmail.com>
2024-06-29 15:56:38 +0700

    Student application

--- src/pages/students/application.tsx
+++ src/pages/students/application.tsx
@@ -1,26 +1,63 @@
+import { useMemo } from 'react';
+import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
 import { TbArrowLeft } from 'react-icons/tb';
-import { GetServerSideProps } from 'next';
 
 import NextHead from '@/common/components/NextHead';
 import NextButton from '@/common/components/NextButton';
 import StudentApplicationForm from '@/modules/students/components/application/StudentApplicationForm';
+import AccountStudentHalfDiscount from '@/modules/account/components/AccountStudentHalfDiscount';
+import AccountStudentFreeDiscount from '@/modules/account/components/AccountStudentFreeDiscount';
+import AccountStudentCantReapply from '@/modules/account/components/AccountStudentCantReapply';
 
+import { StudentInstitutionLevel } from '@/modules/students/lib/StudentInstitutionLevel';
 import { STUDENT_APPLICATION_SEO_SCHEMA } from '@/modules/students/lib/StudentApplicationSeoSchema';
 import { getStudentApplicationPageServerProps } from '@/modules/students/lib/getStudentApplicationPageServerSideProps';
+import { IStudent } from '@/common/lib/api/students/interfaces';
 
 export const getServerSideProps: GetServerSideProps = getStudentApplicationPageServerProps;
 
-const StudentApplication = () => {
+interface StudentApplicationProps {
+  student: IStudent
+}
+
+const StudentApplication = (props: StudentApplicationProps) => {
+  const { student } = props;
   const router = useRouter();
 
+  const isFreeDiscount = useMemo(() => {
+    const currentTime = new Date().getTime();
+    const freeDiscTime = new Date(student.free_discount_end_at).getTime();
+    return freeDiscTime > currentTime;
+  }, [student]);
+  const isHalfDiscount = useMemo(() => {
+    const currentTime = new Date().getTime();
+    const halfDiscTime = new Date(student.half_discount_end_at).getTime();
+    return halfDiscTime > currentTime;
+  }, [student]);
+  if (
+    !isFreeDiscount &&
+    !isHalfDiscount &&
+    student.institution_level === StudentInstitutionLevel.College
+  ) {
+    return <AccountStudentCantReapply />;
+  }
+
+  if (isFreeDiscount) {
+    return <AccountStudentFreeDiscount/>;
+  }
+
+  if (isHalfDiscount) {
+    return <AccountStudentHalfDiscount />;
+  }
+
   return (
-  <div className=' p-4 lg:w-[400px] lg:mx-auto'>
-    <NextHead pagesSchema={STUDENT_APPLICATION_SEO_SCHEMA} />
-    <h1 className='text-2xl font-bold text-center mb-4'>Student Plan Application</h1>
+    <div className=' p-4 lg:w-[400px] lg:mx-auto'>
+      <NextHead pagesSchema={STUDENT_APPLICATION_SEO_SCHEMA} />
+      <h1 className='text-2xl font-bold text-center mb-4'>Student Plan Application</h1>
       <h2 className='text-center mb-4'>Enter your student information to get <b>free & unlimited</b> use of Languageai</h2>
       <StudentApplicationForm />
-      <NextButton variant='outline' className='border-transparent p-2' onClick={()=> router.back()}>
+      <NextButton variant='outline' className='border-transparent p-2' onClick={() => router.back()}>
         <TbArrowLeft />
         <span>Back</span>
       </NextButton>
