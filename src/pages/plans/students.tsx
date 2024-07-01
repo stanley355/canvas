d06f993fa1e359f6bf2ea3950d4590b666546a85
Stanley Winata <winatastanley355@gmail.com>
2024-07-01 12:26:58 +0700

    Premium students

--- src/pages/plans/students.tsx
+++ src/pages/plans/students.tsx
@@ -1,8 +1,12 @@
 import { GetServerSideProps } from "next";
+import NextHead from "@/common/components/NextHead";
+
 import { getPlanStudentPageServerProps } from "@/modules/plans/lib/getPlanStudentPageServerProps";
-import { IStudent } from "@/common/lib/api/students/interfaces";
 import PlanStudentsFeatures from "@/modules/plans/components/students/PlanStudentsFeatures";
 import PlanStudentsForm from "@/modules/plans/components/students/PlanStudentsForm";
+
+import { IStudent } from "@/common/lib/api/students/interfaces";
+import { IDatoPagesSchema } from "@/common/lib/api/dato/interfaces";
 
 export const getServerSideProps: GetServerSideProps = getPlanStudentPageServerProps;
 
@@ -13,8 +17,24 @@ interface StudentsPlansProps {
 const StudentsPlans = (props: StudentsPlansProps) => {
   const {student} = props;
 
+  const schema: IDatoPagesSchema = {
+    _updatedAt: "",
+    slug: "/plans/students/",
+    keywords: "",
+    faq: [],
+    seo: {
+      title: `Student Plans`,
+      description: "Enjoy 1 year unlimited access and 50% off on the 2nd year",
+      image: {
+        alt: "Languageai.id",
+        url: "/images/languageai/logo.png",
+      },
+    },
+  };
+
   return (
     <div className="container mt-14 lg:mt-0 lg:w-1/3 pb-8">
+      <NextHead pagesSchema={schema} />
       <div className="bg-yellow-500 p-4 mb-4">
         Your free student plan has ended on <i>{new Date(student.free_discount_end_at).toLocaleDateString('id-ID')}</i>, 
         but you can enjoy student pricing <b>(50% discount)</b> until <i>{new Date(student.half_discount_end_at).toLocaleDateString('id-id')}</i>
