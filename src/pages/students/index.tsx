0c8afbee4044cc113a53405f0a8d83737120bada
Stanley Winata <winatastanley355@gmail.com>
2024-07-01 15:21:47 +0700

    Students hero

--- src/pages/students/index.tsx
+++ src/pages/students/index.tsx
@@ -1,14 +1,13 @@
 import { GetStaticProps } from "next";
 
-// import StudentsHero from "@/modules/students/components/StudentsHero";
+import NextHead, { NextHeadProps } from "@/common/components/NextHead";
+import StudentsHero from "@/modules/students/components/home/StudentsHero";
 
 import { fetchDatoCms } from "@/common/lib/api/dato/fetchDatoCms";
 import { getPagesSchema } from "@/common/lib/api/gql";
 // import StudentsHeroSupport from "@/modules/students/components/StudentsHeroSupport";
 // import StudentsAssignmentBenefit from "@/modules/students/components/StudentsAssignmentBenefit";
 // import StudentsBoostGrades from "@/modules/students/components/StudentsBoostGrades";
-import NextHead, { NextHeadProps } from "@/common/components/NextHead";
-import StudentsHero from "@/modules/students/components/home/StudentsHero";
 
 interface IStudents {
   datoCmsData: NextHeadProps;
@@ -18,7 +17,7 @@ const Students = (props: IStudents) => {
   const { datoCmsData } = props;
 
   return (
-    <div className="mt-14">
+    <div className="mt-14 lg:mt-0">
       <NextHead pagesSchema={datoCmsData.pagesSchema} />
       <StudentsHero />
     </div>
