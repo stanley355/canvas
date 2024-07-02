4c8aaf88a61be2aac3a73f50373dfc9a482aa467
Stanley Winata <winatastanley355@gmail.com>
2024-07-02 15:17:37 +0700

    Translate Provider

--- src/modules/translate/lib/translateStates.ts
+++ src/modules/translate/lib/translateStates.ts
@@ -2,9 +2,13 @@
 export interface ITranslateStates {
   firstLanguage: string;
   secondLanguage: string;
+  n: 1 | 2 | 3;
+  temperature: 0.5 | 1 | 1.5;
 }
 
 export const TRANSLATE_STATES: ITranslateStates = {
   firstLanguage: "",
-  secondLanguage: ""
+  secondLanguage: "",
+  n: 1,
+  temperature: 1
 }
