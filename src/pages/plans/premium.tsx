a8a1dc8affcb57f9eb533667c2ba3bda94e92487
Stanley Winata <winatastanley355@gmail.com>
2024-07-02 11:35:05 +0700

    Plans premium

--- src/pages/plans/premium.tsx
+++ src/pages/plans/premium.tsx
@@ -1,17 +1,32 @@
 import { GetServerSideProps } from "next";
+
 import { getPlanPremiumPageServerProps } from "@/modules/plans/lib/getPlanPremiumPageServerProps";
-import PlanPremiumCard from "@/modules/plans/components/PlanPremiumCard";
-import PlanPremiumForm from "@/modules/plans/components/PlanPremiumForm";
+import { IDatoPagesSchema } from "@/common/lib/api/dato/interfaces";
+
+import NextHead from "@/common/components/NextHead";
 
 export const getServerSideProps: GetServerSideProps = getPlanPremiumPageServerProps;
 
 const PremiumPlans = () => {
+  const schema: IDatoPagesSchema = {
+    _updatedAt: "",
+    slug: "/plans",
+    keywords: "",
+    faq: [],
+    seo: {
+      title: `Premium Plans`,
+      description: "Enjoy Languageai.id Premium Plans for unlimited access",
+      image: {
+        alt: "Languageai.id",
+        url: "/images/languageai/logo.png",
+      },
+    },
+  };
+
   return (
-    <div className="container px-4 mx-auto mt-16 lg:mt-4">
-      <div className="lg:w-1/3 lg:mx-auto">
-        <PlanPremiumCard />
-        <PlanPremiumForm />
-      </div>
+    <div className="container mt-14 lg:mt-0 lg:w-1/3 pb-8">
+      <NextHead pagesSchema={schema} />
+
     </div>
   );
 };
