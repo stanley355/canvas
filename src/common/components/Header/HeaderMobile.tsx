d179e5a99670d04f11323b619b11a48ec6da75ea
Stanley Winata <winatastanley355@gmail.com>
2024-07-02 15:29:39 +0700

    Fix languageai header mobile

--- src/common/components/Header/HeaderMobile.tsx
+++ src/common/components/Header/HeaderMobile.tsx
@@ -20,7 +20,7 @@ const HeaderMobile = (props: IHeaderMobile) => {
           alt="languageai.id"
           width={100}
           height={50}
-          className="w-auto h-full"
+          className="w-1/2 h-full"
         />
       </Link>
       <NextButton
