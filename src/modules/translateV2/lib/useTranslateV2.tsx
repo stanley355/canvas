245efc33b074ccca38430c51ce817ea084221564
Stanley Winata <winatastanley355@gmail.com>
2024-07-04 10:49:05 +0700

    Remove unused code

--- src/modules/translateV2/lib/useTranslateV2.tsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { TranslateContextV2 } from "./translateContextV2";
-
-export const useTranslateV2 = () => {
-  const context = useContext(TranslateContextV2);
-  return context;
-};
