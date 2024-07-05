46d9db34f5bf961edf610556e56c9a624ec5c465
Stanley Winata <winatastanley355@gmail.com>
2024-07-05 12:30:16 +0700

    Remove unused code

--- src/modules/grammar-check/lib/grammarCheckContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-import { GRAMMAR_CHECK_STATES } from "./grammarCheckStates";
-import { IGrammarCheckReducerAction } from "./grammarCheckReducer";
-
-export const GrammarCheckContext = createContext({
-  grammarCheckStates: GRAMMAR_CHECK_STATES,
-  dispatch: (_action: IGrammarCheckReducerAction) => {},
-});
