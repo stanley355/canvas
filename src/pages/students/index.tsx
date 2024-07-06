b72860943a16f644e151eb0a89f2481d72c420a0
Stanley Winata <winatastanley355@gmail.com>
2024-07-06 11:12:07 +0700

    Update student page

--- src/pages/students/index.tsx
+++ src/pages/students/index.tsx
@@ -5,7 +5,6 @@ import StudentsHero from "@/modules/students/components/home/StudentsHero";
 
 import { fetchDatoCms } from "@/common/lib/api/dato/fetchDatoCms";
 import { getPagesSchema } from "@/common/lib/api/gql";
-import AccountFreePlanTable from "@/modules/account/components/AccountFreePlanTable";
 
 interface IStudents {
   datoCmsData: NextHeadProps;
@@ -18,12 +17,6 @@ const Students = (props: IStudents) => {
     <div className="mt-14 lg:mt-0">
       <NextHead pagesSchema={datoCmsData.pagesSchema} />
       <StudentsHero />
-      <div className="lg:w-1/2 lg:mx-auto p-4 lg:px-0">
-        <h3 className="text-center text-2xl font-bold mb-4">
-          Comparing Student Plan to other plans
-        </h3>
-        <AccountFreePlanTable />
-      </div>
     </div>
   );
 };
