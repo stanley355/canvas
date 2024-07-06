baf0e3ff948427d55f9e8652424eb8ec16261110
Stanley Winata <winatastanley355@gmail.com>
2024-07-06 10:55:46 +0700

    Remove unused package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import FirebaseProvider from "@/modules/firebase/components/FirebaseProvider";
 import AppProvider from "@/modules/app/components/AppProvider";
 import Layout from "@/common/components/Layout";
 import "@/styles/globals.scss";
-import "@/styles/shadcn.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
