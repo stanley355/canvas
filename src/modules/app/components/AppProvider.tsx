5e84b7e212e2a8eae27991064e55c13cd564d92c
Stanley Winata <winatastanley355@gmail.com>
2024-07-08 12:15:44 +0700

    Add clarity

--- src/modules/app/components/AppProvider.tsx
+++ src/modules/app/components/AppProvider.tsx
@@ -1,4 +1,5 @@
 import dynamic from "next/dynamic";
+import Script from "next/script";
 import { useReducer } from "react";
 import { AppContext } from "./AppContext";
 import { appReducer } from "../lib/appReducer";
@@ -20,6 +21,17 @@ const AppProvider = (props: IAppProvider) => {
   const [appStates, appDispatch] = useReducer(appReducer, APP_STATES);
   return (
     <AppContext.Provider value={{ appStates, appDispatch }}>
+      <Script
+        type="text/javascript"
+        id="microsoft-clarity"
+        strategy="afterInteractive"
+      >
+        {`(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "m0voqp1w10");`}
+      </Script>
       {props.children}
       {appStates.showLoginModal && <LoginModal />}
       {appStates.showMonthlyLimitModal && (
