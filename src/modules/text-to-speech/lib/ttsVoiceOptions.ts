6b691ce5d560612a85afb4e6ab2ec36d54b564fb
Stanley Winata <winatastanley355@gmail.com>
2024-07-10 07:44:30 +0700

    Tts header

---
+++ src/modules/text-to-speech/lib/ttsVoiceOptions.ts
@@ -0,0 +1,29 @@
+import { IOption } from "@/common/components/interfaces";
+import { TextToSpeechVoice } from "@/common/lib/api/prompts/fetchPromptsTts";
+
+export const TTS_VOICE_OPTIONS : IOption[] = [
+  {
+    label: "Voice: Alpha",
+    value: TextToSpeechVoice.Alloy,
+  },
+  {
+    label: "Voice: Beta",
+    value: TextToSpeechVoice.Echo,
+  },
+  {
+    label: "Voice: Gamma",
+    value: TextToSpeechVoice.Fable,
+  },
+  {
+    label: "Voice: Delta",
+    value: TextToSpeechVoice.Nova,
+  },
+  {
+    label: "Voice: Epsilon",
+    value: TextToSpeechVoice.Onyx,
+  },
+  {
+    label: "Voice: Zeta",
+    value: TextToSpeechVoice.Shimmer,
+  },
+]
