8369092231c9174fbd437271c2cef5366657e160
Stanley Winata <winatastanley355@gmail.com>
2024-07-19 07:36:09 +0700

    Update translate prompt

--- src/modules/translate/lib/createTranslateSystemContent.ts
+++ src/modules/translate/lib/createTranslateSystemContent.ts
@@ -3,10 +3,12 @@ export const createTranslateSystemContent = (
   secondLanguage: string
 ) => {
   if (firstLanguage === "") {
-    const content = `You are a translator. You will be provided with a text, and your task is translate the text to ${secondLanguage}.`;
+    const content = `You are a translator. You will be provided with a text, and your task is translate the text to ${secondLanguage}. 
+    If it was an idiom, explain the meaning of the idiom. If it's not an idiom, just translate the text literally.`;
     return content;
   }
 
-  const content = `You are a translattor. You will be provided with a text, and your task is to translate the text from ${firstLanguage} to ${secondLanguage}.`;
+  const content = `You are a translator. You will be provided with a text, and your task is to translate the text from ${firstLanguage} to ${secondLanguage}. 
+  If it was an idiom, explain the meaning of the idiom. If it's not an idiom, just translate the text literally.`;
   return content;
 };
