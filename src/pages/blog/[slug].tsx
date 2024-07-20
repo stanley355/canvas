e9717a684f1442106d79d03ae43c117db2419d61
Stanley Winata <winatastanley355@gmail.com>
2024-07-20 15:03:18 +0700

    Blog

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,13 +1,12 @@
 import { GetStaticPaths, GetStaticProps } from "next";
 import { useRouter } from "next/router";
-import { TbProgress } from "react-icons/tb";
+import { TbArrowLeft, TbProgress } from "react-icons/tb";
 
 import NextHead from "@/common/components/NextHead";
 import { getBlogSlugStaticPaths } from "@/modules/blog/lib/getBlogSlugStaticPath";
 import { getBlogSlugStaticProps } from "@/modules/blog/lib/getBlogSlugStaticProps";
 import { IDatoBlogSchema } from "@/common/lib/api/dato/interfaces";
-import BlogSlugMain from "@/modules/blog/components/slug/BlogSlugMain";
-import BlogSlugRelatedArticles from "@/modules/blog/components/slug/BlogSlugRelatedArticles";
+import NextButton from "@/common/components/NextButton";
 
 export const getStaticPaths: GetStaticPaths = getBlogSlugStaticPaths;
 export const getStaticProps: GetStaticProps = getBlogSlugStaticProps;
@@ -31,9 +30,31 @@ const BlogSlug = ({ blogSchema }: BlogSlugProps) => {
   return (
     <div className="mt-16 lg:mt-4 text-sm pb-8">
       <NextHead pagesSchema={blogSchema.blog} />
-      <div className="lg:container lg:mx-auto lg:grid lg:grid-cols-[66%_33%] gap-[1%]">
-        <BlogSlugMain blogSchema={blogSchema} />
-        <BlogSlugRelatedArticles blogSchema={blogSchema} />
+      <div className="lg:w-1/3 lg:mx-auto lg:shadow-inner lg:rounded-lg">
+        <div className="p-4">
+          <div className="mb-2">
+            {new Date(blogSchema.blog._updatedAt).toLocaleDateString("id-ID")}
+          </div>
+          <h1 className="text-lg font-bold">{blogSchema.blog.seo.title}</h1>
+        </div>
+        <img
+          src={blogSchema.blog.heroImage.url}
+          alt={blogSchema.blog.heroImage.title}
+          width={blogSchema.blog.heroImage.width}
+          height={blogSchema.blog.heroImage.height}
+          className="w-full h-auto lg:w-1/2 lg:h-1/2 lg:rounded-lg lg:mx-4"
+          loading="eager"
+        />
+
+        <div
+          className="p-4 [&_a]:underline [&_p]:mb-4 [&_ul]:mb-4 [&_ol]:mb-4 [&_ul]:list-inside [&_ol]:list-inside [&_ul]:list-disc [&_ol]:list-decimal"
+          dangerouslySetInnerHTML={{ __html: blogSchema.blog.content }}
+        />
+
+        <NextButton className="ml-4 rounded-3xl" variant="outline" onClick={()=> router.back()}>
+          <TbArrowLeft />
+          Back
+        </NextButton>
       </div>
     </div>
   );
