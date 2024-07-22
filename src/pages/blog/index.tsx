9dff02a1a9181f99037ea5ca1ddf66ae8395610e
Stanley Winata <winatastanley355@gmail.com>
2024-07-22 13:22:35 +0700

    Blog homepage  design

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,3 +1,5 @@
+import NextLink from "@/common/components/NextLink";
+import { cn } from "@/common/lib/cn";
 import {
   getBlogServerSideProps,
   IDatoBlogHome,
@@ -11,32 +13,33 @@ export const getServerSideProps: GetServerSideProps = getBlogServerSideProps;
 
 const Blog = ({ allBlogs }: IDatoBlogHome) => {
   return (
-    <div className="my-16 text-sm px-4 lg:px-0 lg:my-0 lg:container lg:mx-auto">
+    <div className="mt-16 text-sm px-4 lg:mt-0 pb-8">
       <div className="flex border border-brand-primary items-center gap-1 lg:justify-center rounded-lg p-2 bg-blue-100 mb-4 w-fit">
         <FaBlog />
         Blog
       </div>
-      <div className="lg:grid lg:grid-cols-3 lg:gap-4 ">
-        {allBlogs?.map(({ seo, slug }) => (
-          <Link
+      <div className="grid grid-cols-2 gap-2 lg:flex lg:flex-wrap">
+        {allBlogs?.map(({ seo, slug }, index: number) => (
+          <NextLink
             href={`/blog/${slug}`}
-            className="flex h-40 mb-4 gap-4 shadow p-4 rounded-lg hover:underline hover:bg-gradient-to-b from-white to-blue-100"
+            className={cn("w-full gap-4 hover:underline h-fit mb-4", index % 2 ? 'lg:flex-[1_1_15%] lg:max-w-[15%]': 'lg:flex-[1_1_20%] lg:max-w-[20%]' )}
             key={seo.title}
+            variant="none"
           >
             <img
               src={seo.image.url}
               alt={seo.image.alt}
               width={seo.image.width}
               height={seo.image.height}
-              className="w-[33%] rounded-lg"
+              className="w-full h-auto rounded-lg mb-2 object-cover"
             />
             <div className="overflow-hidden text-ellipsis">
-              <div className="font-bold text-base mb-2">{seo.title}</div>
-              <div className="text-gray-700 text-xs text-justify">
+              <div className="font-semibold text-base">{seo.title}</div>
+              <div className="text-gray-700 text-xs ">
                 {seo.description}
               </div>
             </div>
-          </Link>
+          </NextLink>
         ))}
       </div>
     </div>
