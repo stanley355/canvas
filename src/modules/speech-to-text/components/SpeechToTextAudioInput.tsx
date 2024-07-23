658d4f2ef7570f266a11d4833a3fbdd4294890e5
Stanley Winata <winatastanley355@gmail.com>
2024-07-23 13:39:58 +0700

    Fix speech to text reupload every click

--- src/modules/speech-to-text/components/SpeechToTextAudioInput.tsx
+++ src/modules/speech-to-text/components/SpeechToTextAudioInput.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useContext, useRef, useState, memo } from "react";
+import { FormEvent, useContext, useRef, useState, memo, ChangeEvent } from "react";
 import { TbMicrophone, TbProgress, TbUpload } from "react-icons/tb";
 import { toast } from "react-toastify";
 import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
@@ -6,7 +6,6 @@ import { decode, JwtPayload } from "jsonwebtoken";
 import Cookies from "js-cookie";
 
 import NextButton from "@/common/components/NextButton";
-import { SpeechToTextContext } from "./SpeechToTextContext";
 import { AppContext } from "@/modules/app/components/AppContext";
 
 import { cn } from "@/common/lib/cn";
@@ -27,8 +26,10 @@ const SpeechToTextAudioInput = (props: SpeechToTextAudioInputProps) => {
   const { temperature, language, timestamp_granularities } = speechToTextStates;
 
   const inputRef = useRef<any>(null);
-  const [file, setFile] = useState<File | null>(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [isUploading, setIsUploading] = useState(false);
+  const [fileName, setFileName] = useState("");
+  const [downloadURL, setDownloadURL] = useState("");
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -39,17 +40,8 @@ const SpeechToTextAudioInput = (props: SpeechToTextAudioInputProps) => {
       return;
     }
 
-    const target = e.target as any;
-    const audioFile = target.audio.files[0];
-
-    if (!file) {
-      toast.error("Please upload audio file");
-      return;
-    }
-
-    const maxFileSize = 24 * 1024 * 1024; // 24MB
-    if (audioFile.size > maxFileSize) {
-      toast.error("Max file size is 24MB");
+    if (!downloadURL) {
+      toast.error("Please upload a file");
       return;
     }
 
@@ -58,76 +50,100 @@ const SpeechToTextAudioInput = (props: SpeechToTextAudioInputProps) => {
       return;
     }
 
-    sendFirebaseEvent(FIREBASE_EVENT_NAMES.speech_to_text);
     setIsLoading(true);
-    try {
-      const user = decode(token) as JwtPayload;
-      const firebasePath = `audio/transcriptions/${
-        user.id
-      }:${new Date().getTime()}`;
-      const storage = getStorage();
-      const storef = ref(storage, firebasePath);
-      const result = await uploadBytes(storef, audioFile);
-      const downloadURL = await getDownloadURL(result.ref);
+    sendFirebaseEvent(FIREBASE_EVENT_NAMES.speech_to_text);
+    const user = decode(token) as JwtPayload;
+    const req = {
+      user_id: user.id,
+      file_url: downloadURL,
+      temperature,
+      language: language.value,
+      ...(timestamp_granularities && { timestamp_granularities }),
+    };
 
-      const req = {
-        user_id: user.id,
-        file_url: downloadURL,
-        temperature,
-        language: language.value,
-        ...(timestamp_granularities && { timestamp_granularities }),
-      };
+    const transcription = await fetchPromptsTranscriptions(req);
+    setIsLoading(false);
 
-      const transcription = await fetchPromptsTranscriptions(req);
-      if (transcription?.status === 402) {
-        appDispatch({ key: "showMonthlyLimitModal", value: true });
-        return;
-      }
+    if (transcription?.status === 402) {
+      appDispatch({ key: "showMonthlyLimitModal", value: true });
+      return;
+    }
 
-      setIsLoading(false);
+    if (transcription) {
       speechToTextDispatch({ key: "transcription", value: transcription });
       return;
+    }
+
+    toast.error("Server busy, please try again");
+    return;
+  };
+
+  const handleUpload = async (e: ChangeEvent<HTMLInputElement>) => {
+    const token = Cookies.get("token");
+    if (!token) {
+      appDispatch({ key: "showLoginModal", value: true });
+      return;
+    }
+
+    const target = e.target as any;
+    const file = target.files[0];
+    const maxFileSize = 24 * 1024 * 1024; // 24MB
+    if (file.size > maxFileSize) {
+      toast.error("Max file size is 24MB");
+      return;
+    }
+
+    sendFirebaseEvent(FIREBASE_EVENT_NAMES.click.stt_upload);
+    setIsUploading(true);
+    try {
+      const user = decode(token) as JwtPayload;
+      const firebasePath = `audio/transcriptions/${user.id
+        }:${new Date().getTime()}`;
+      const storage = getStorage();
+      const storef = ref(storage, firebasePath);
+      const result = await uploadBytes(storef, file);
+      const downloadURL = await getDownloadURL(result.ref);
+      
+      setFileName(file.name);
+      setDownloadURL(downloadURL);
+      setIsUploading(false);
+      return;
     } catch (error: any) {
-      setIsLoading(false);
+      setIsUploading(false)
       toast.error("Server busy, please try again");
       return;
     }
-  };
+  }
 
   return (
     <form className="px-2 lg:px-0 relative" onSubmit={handleSubmit}>
       <NextButton
         type="button"
-        disabled={isLoading}
-        variant={isLoading ? "disabled" : "outline"}
+        disabled={isLoading || isUploading}
+        variant={isLoading || isUploading ? "disabled" : "outline"}
         className="flex-col justify-center h-40 w-full"
         onClick={() => inputRef.current.click()}
       >
-        {isLoading ? (
+        {isLoading || isUploading ? (
           <TbProgress className="text-3xl animate-spin" />
         ) : (
           <TbUpload className="text-3xl" />
         )}
-        <div className="text-lg">{file ? file.name : "Click to Upload"}</div>
-        <div>
-          {file
-            ? `${
-                Math.floor(file.size / 1024 / 1024) > 1
-                  ? `${Math.floor(file.size / 1024 / 1024)}MB`
-                  : `${Math.floor(file.size / 1024)}KB`
-              }`
-            : "*Max file size 24 MB"}
+        <div className="text-lg">{fileName ? fileName : "Click to Upload"}</div>
+        <div className="text-gray-500">
+          {fileName ? "Click Convert to Continue" : "*Max file size 24 MB"}
         </div>
       </NextButton>
       <NextButton
-        disabled={isLoading}
-        className={cn("absolute right-3 bottom-6 lg:right-1 ", {
-          "border-brand-primary": isLoading,
-        })}
+        disabled={isLoading || isUploading}
+        className={cn("absolute right-3 bottom-6 lg:right-1 ",
+          {
+            "border-brand-primary": isLoading || isUploading,
+          })}
         type="submit"
       >
         <TbMicrophone />
-        <div>{isLoading ? "This may take a while" : "Convert"}</div>
+        <div>{isLoading ? "This may take a while" : isUploading ? "Uploading..." : "Convert"}</div>
       </NextButton>
       <input
         type="file"
@@ -136,17 +152,7 @@ const SpeechToTextAudioInput = (props: SpeechToTextAudioInputProps) => {
         ref={inputRef}
         accept=".mp3, .mp4, .wav"
         className="invisible h-0"
-        onChange={(e) => {
-          const target = e.target as any;
-          const file = target.files[0];
-          const maxFileSize = 24 * 1024 * 1024; // 24MB
-          if (file.size > maxFileSize) {
-            toast.error("Max file size is 24MB");
-            return;
-          }
-          setFile(file);
-          return;
-        }}
+        onChange={handleUpload}
       />
     </form>
   );
