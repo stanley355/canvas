cfe39c82fe70d2bbc2e9d4d8450e8c0fb053aba7
Stanley Winata <winatastanley355@gmail.com>
2024-07-29 11:00:06 +0700

    Translate audo reducer and states

---
+++ src/modules/translate-audio/lib/TranslateAudioReducer.ts
@@ -0,0 +1,14 @@
+import { ITranslateAudioStates } from "./TranslateAudioStates";
+
+export interface ITranslateAudioReducerAction {
+  key: keyof ITranslateAudioStates;
+  value: any;
+}
+
+export const translateAudioReducer = (
+  state: Record<keyof ITranslateAudioStates, any>,
+  action: ITranslateAudioReducerAction 
+) => {
+  state[action.key] = action.value;
+  return structuredClone(state);
+};
