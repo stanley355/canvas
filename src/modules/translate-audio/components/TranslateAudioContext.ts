6c0702465ce014a35ff297b8fe43f68e4a0e9529
Stanley Winata <winatastanley355@gmail.com>
2024-07-29 11:14:37 +0700

    Translate audo provider

---
+++ src/modules/translate-audio/components/TranslateAudioContext.ts
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+import { ITranslateAudioStates, TRANSLATE_AUDIO_STATES } from "../lib/TranslateAudioStates";
+import { ITranslateAudioReducerAction } from "../lib/TranslateAudioReducer";
+
+interface ITranslateAudioContext {
+  translateAudioStates: ITranslateAudioStates;
+  translateAudioDispatch: React.Dispatch<ITranslateAudioReducerAction>;
+}
+
+export const TranslateAudioContext = createContext<ITranslateAudioContext>({
+  translateAudioStates: TRANSLATE_AUDIO_STATES,
+  translateAudioDispatch: () => {},
+});
