22eec7a57f7e9139098b9ccc18a926f8f986bfef
stan <winatastanley355@gmail.com>
2024-08-07 07:35:39 +0700

    refactor: Button variant styling

--- src/common/components/Button/variants.ts
+++ src/common/components/Button/variants.ts
@@ -1,6 +1,9 @@
 export type TButtonVariants = {
   default: string;
   secondary: string;
+  danger: string;
+  outline: string;
+  ghost: string;
   none: string;
 };
 
@@ -9,6 +12,9 @@ const BASE_BUTTON_CLASSNAME =
 
 export const BUTTON_VARIANTS: TButtonVariants = {
   default: `${BASE_BUTTON_CLASSNAME} bg-primary hover:bg-blue-800`,
-  secondary: `${BASE_BUTTON_CLASSNAME} bg-blue-600 hover:bg-blue-700`,
+  secondary: `${BASE_BUTTON_CLASSNAME} bg-blue-700 hover:bg-blue-600`,
+  danger: `${BASE_BUTTON_CLASSNAME} bg-red-600 hover:bg-red-500`,
+  outline: `${BASE_BUTTON_CLASSNAME} bg-white hover:bg-slate-200 border`,
+  ghost: `${BASE_BUTTON_CLASSNAME} bg-white hover:bg-slate-200`,
   none: "",
 };
