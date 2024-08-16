04d13be3f32b75197d5d8f0fc82502a3b832d207
stan <winatastanley355@gmail.com>
2024-08-16 21:06:44 +0700

    refactor: Modal props from interface to type

--- src/common/components/Modal.tsx
+++ src/common/components/Modal.tsx
@@ -1,12 +1,11 @@
-interface ModalProps {
+type TModalProps = {
   children: React.ReactNode;
 }
 
-const Modal = (props: ModalProps) => {
-  const { children } = props;
+const Modal = ({ children }: TModalProps) => {
 
   return (
-    <div className="fixed top-0 left-0 w-full h-full bg-[#17255455] z-50 animate-visible-forward">
+    <div className="fixed top-0 left-0 z-50 w-full h-full bg-black/50 animate-visible-forward">
       {children}
     </div>
   );
