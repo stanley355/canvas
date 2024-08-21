bc933de8dfb8c8a8afd6e76d42751559913a5c38
Stanley Winata <winatastanley355@gmail.com>
2024-08-21 16:56:51 +0700

    feat: New home features

--- src/modules/home/components/HomeHero.tsx
+++ src/modules/home/components/HomeHero.tsx
@@ -1,8 +1,28 @@
+import Link from "next/link";
+import Textarea from "@/common/components/Textarea";
+
 const HomeHero = () => {
   return (
-    <section className="py-8">
+    <section className="container h-screen px-4 py-8 snap-center">
       <h1 className="text-xl font-bold text-center">#1 Language Processor</h1>
-      <h2 className="text-lg text-center">Process all text and audio with AI</h2>
+      <h2 className="mb-4 text-center">Check, translate, convert</h2>
+      <div
+        onSubmit={(e) => e.preventDefault()}
+        className="flex flex-col mb-4 border rounded-md h-60 animate-visible-forward"
+      >
+        <Textarea
+          className="flex-1 w-full mx-auto border-none resize-none focus-visible:outline-none"
+          placeholder="Enter your text"
+        />
+        <div className="flex items-center justify-end p-2">
+          <Link href="#home_features">Next</Link>
+        </div>
+      </div>
+      <div className="flex items-center justify-center">
+        <Link href="#home_features" className="underline text-brand-primary">
+          I want to convert audio
+        </Link>
+      </div>
     </section>
   );
 };
