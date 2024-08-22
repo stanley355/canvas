8ff451f667dc14e87f4566e2b937870e82ce47a7
stan <winatastanley355@gmail.com>
2024-08-22 20:16:25 +0700

    feat: Home hero background

--- src/modules/home/components/HomeHero.tsx
+++ src/modules/home/components/HomeHero.tsx
@@ -1,27 +1,28 @@
 import Link from "next/link";
 import Textarea from "@/common/components/Textarea";
+import Header from "@/common/components/Header";
 
 const HomeHero = () => {
   return (
-    <section className="container h-screen px-4 py-8 snap-center">
-      <h1 className="text-xl font-bold text-center">#1 Language Processor</h1>
-      <h2 className="mb-4 text-center">Check, translate, convert</h2>
-      <div
-        onSubmit={(e) => e.preventDefault()}
-        className="flex flex-col mb-4 border rounded-md h-60 animate-visible-forward"
-      >
-        <Textarea
-          className="flex-1 w-full mx-auto border-none resize-none focus-visible:outline-none"
-          placeholder="Enter your text"
-        />
-        <div className="flex items-center justify-end p-2">
-          <Link href="#home_features">Next</Link>
+    <section className="h-screen snap-center bg-[url('/images/home/hero_bg.jpg')] bg-cover lg:bg-bottom">
+      <Header pathname="/"/>
+      <div className="container">
+        <h1 className="text-xl font-bold text-center">#1 Language Processor</h1>
+        <h2 className="mb-4 text-center">Check, translate, convert</h2>
+        <div className="flex flex-col mb-4 bg-white bg-opacity-50 border rounded-md h-60 animate-visible-forward">
+          <Textarea
+            className="flex-1 w-full mx-auto border-none resize-none focus-visible:outline-none"
+            placeholder="Enter your text"
+          />
+          <div className="flex items-center justify-end p-2">
+            <Link href="#home_features" className="bg-brand-primary">Next</Link>
+          </div>
         </div>
-      </div>
-      <div className="flex items-center justify-center">
-        <Link href="#home_features" className="underline text-brand-primary">
-          I want to convert audio
-        </Link>
+        <div className="flex items-center justify-center">
+          <Link href="#home_features" className="underline text-brand-primary">
+            I want to convert audio
+          </Link>
+        </div>
       </div>
     </section>
   );
