71fc134825c4c831fe9eb6320cb49816e570d2f3
stan <winatastanley355@gmail.com>
2024-08-23 08:00:43 +0700

    style: Prettier

--- src/modules/home/components/HomeFeatures.tsx
+++ src/modules/home/components/HomeFeatures.tsx
@@ -1,16 +1,20 @@
 import HomeFeaturesAccordion from "./HomeFeaturesAccordion";
 
 const HomeFeatures = () => {
-  return <section className="h-screen px-4 py-8 lg:px-0 snap-always snap-center bg-slate-100">
-    <div className="container">
-      <h3 className="mb-4 text-lg font-semibold lg:text-2xl">Explore what you can do with our AI</h3>
-      <div className="divide-y divide-black border-y border-y-black">
-        <HomeFeaturesAccordion />
-        <HomeFeaturesAccordion />
-        <HomeFeaturesAccordion />
+  return (
+    <section className="h-screen px-4 py-8 lg:px-0 snap-always snap-center bg-slate-100">
+      <div className="container">
+        <h3 className="mb-4 text-lg font-semibold lg:text-2xl">
+          Explore what you can do with our AI
+        </h3>
+        <div className="divide-y divide-black border-y border-y-black">
+          <HomeFeaturesAccordion />
+          <HomeFeaturesAccordion />
+          <HomeFeaturesAccordion />
+        </div>
       </div>
-    </div>
-  </section>;
+    </section>
+  );
 };
 
 export default HomeFeatures;
--- src/modules/home/components/HomeFeaturesAccordion.tsx
+++ src/modules/home/components/HomeFeaturesAccordion.tsx
@@ -1,20 +1,23 @@
-import Button from '@/common/components/Button'
-import React, { useState } from 'react'
-import { TbPlus } from 'react-icons/tb'
+import Button from "@/common/components/Button";
+import React, { useState } from "react";
+import { TbPlus } from "react-icons/tb";
 
 const HomeFeaturesAccordion = () => {
   const [showContent, setShowcontent] = useState(false);
   return (
     <div>
-      <Button variant="ghost" className='justify-between w-full text-lg bg-transparent'>
+      <Button
+        variant="ghost"
+        className="justify-between w-full text-lg bg-transparent"
+      >
         <span>Check grammar and spelling</span>
         <TbPlus />
       </Button>
-    <div className='px-4 py-2 animate-visible-forward'>
-      Checkbot is the answer
+      <div className="px-4 py-2 animate-visible-forward">
+        Checkbot is the answer
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default HomeFeaturesAccordion
+export default HomeFeaturesAccordion;
