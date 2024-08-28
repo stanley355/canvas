708207f748f058771709cca4ae2c096c8b9620d6
stan <winatastanley355@gmail.com>
2024-08-28 14:56:15 +0700

    style: Prettier

--- src/modules/home/components/HomeFinal.tsx
+++ src/modules/home/components/HomeFinal.tsx
@@ -7,21 +7,22 @@ const HomeFinal = () => {
   const router = useRouter();
   const [isLogin, setIsLogin] = useState(false);
 
-  useEffect(()=> {
-    const token = Cookies.get('token');
+  useEffect(() => {
+    const token = Cookies.get("token");
     if (token) setIsLogin(true);
-  }, [isLogin])
+  }, [isLogin]);
 
   if (isLogin) {
-    return <></>
+    return <></>;
   }
 
   return (
     <section className="container h-screen py-4 mx-auto lg:py-12">
-      <h3 className="text-3xl font-semibold text-center">Skip the talk and use it directly</h3>
+      <h3 className="text-3xl font-semibold text-center">
+        Skip the talk and use it directly
+      </h3>
       <div className="mx-auto lg:w-1/2">
-
-      <LoginMain onBackClick={()=> router.back()} />  
+        <LoginMain onBackClick={() => router.back()} />
       </div>
     </section>
   );
